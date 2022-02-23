af3d6c24a99fd5e8545ab3c16e995a78fa4ffa9f
Timemanchik <timemanchik@mail.ru>
2022-02-23 03:13:37 +0300

    Homework02 refine Array my_map, my_select, my_each

--- HW02/siarhei_pratasevich/hw02.rb
+++ HW02/siarhei_pratasevich/hw02.rb
@@ -4,6 +4,7 @@ module MyArrayMethods
   refine Array do
     def my_map
 <<<<<<< HEAD
+<<<<<<< HEAD
       return enum_for(:map) unless block_given?
 
 =======
@@ -23,6 +24,24 @@ module MyArrayMethods
     def my_each
       return enum_for(:each) unless block_given?
 
+=======
+      result = []
+      size.times do |index|
+        result.push(yield(self[index]))
+      end
+      result
+    end
+
+    def my_select
+      result = []
+      size.times do |index|
+        result.push(self[index]) if yield(self[index])
+      end
+      result
+    end
+
+    def my_each
+>>>>>>> Homework02 refine Array my_map, my_select, my_each
       size.times do |index|
         yield(self[index])
       end
