ad6bf597905fcd196b665324bd651b4562e5ceee
Timemanchik <timemanchik@mail.ru>
2022-02-23 21:31:59 +0300

    fixed nil bug in HW02

--- HW02/siarhei_pratasevich/hw02.rb
+++ HW02/siarhei_pratasevich/hw02.rb
@@ -2,15 +2,18 @@
 
 module MyArrayMethods
   refine Array do
-    def my_map
+    def my_map(&block)
+      return nil if block == nil
       reduce([]) { |result, array_element| result.push(yield(array_element)) }
     end
 
-    def my_select
+    def my_select(&block)
+      return nil if block == nil
       reduce([]) { |result, array_element| yield(array_element) ? result.push(array_element) : result }
     end
 
-    def my_each
+    def my_each(&block)
+      return nil if block == nil
       size.times do |index|
         yield(self[index])
       end
