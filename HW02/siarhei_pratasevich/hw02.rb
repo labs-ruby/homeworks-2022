c2ed68fc45558d77ab84b6c0dcb1cbd1a00d788b
Timemanchik <timemanchik@mail.ru>
2022-02-23 10:55:03 +0300

    HW02 modifed refine Array red method

--- HW02/siarhei_pratasevich/hw02.rb
+++ HW02/siarhei_pratasevich/hw02.rb
@@ -3,14 +3,20 @@
 module MyArrayMethods
   refine Array do
     def my_map
+<<<<<<< HEAD
       return enum_for(:map) unless block_given?
 
+=======
+>>>>>>> HW02 modifed refine Array red method
       reduce([]) { |result, array_element| result.push(yield(array_element)) }
     end
 
     def my_select
+<<<<<<< HEAD
       return enum_for(:select) unless block_given?
 
+=======
+>>>>>>> HW02 modifed refine Array red method
       reduce([]) { |result, array_element| yield(array_element) ? result.push(array_element) : result }
     end
 
