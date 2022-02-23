2ba9ea437ae8a276f8a1ccce9ef79a704fca440d
Timemanchik <timemanchik@mail.ru>
2022-02-23 10:55:03 +0300

    HW02 modifed refine Array red method

--- HW02/siarhei_pratasevich/hw02.rb
+++ HW02/siarhei_pratasevich/hw02.rb
@@ -2,46 +2,21 @@
 
 module MyArrayMethods
   refine Array do
-    def my_map
-<<<<<<< HEAD
-<<<<<<< HEAD
-      return enum_for(:map) unless block_given?
+    def my_map(&block)
+      return nil if block.nil?
 
-=======
->>>>>>> HW02 modifed refine Array red method
       reduce([]) { |result, array_element| result.push(yield(array_element)) }
     end
 
-    def my_select
-<<<<<<< HEAD
-      return enum_for(:select) unless block_given?
+    def my_select(&block)
+      return nil if block.nil?
 
-=======
->>>>>>> HW02 modifed refine Array red method
       reduce([]) { |result, array_element| yield(array_element) ? result.push(array_element) : result }
     end
 
-    def my_each
-      return enum_for(:each) unless block_given?
+    def my_each(&block)
+      return nil if block.nil?
 
-=======
-      result = []
-      size.times do |index|
-        result.push(yield(self[index]))
-      end
-      result
-    end
-
-    def my_select
-      result = []
-      size.times do |index|
-        result.push(self[index]) if yield(self[index])
-      end
-      result
-    end
-
-    def my_each
->>>>>>> Homework02 refine Array my_map, my_select, my_each
       size.times do |index|
         yield(self[index])
       end
