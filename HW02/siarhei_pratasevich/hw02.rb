c6aa22dc25d168f2be1266ed5cdcf7589e5be4fe
Timemanchik <timemanchik@mail.ru>
2022-02-24 03:36:45 +0300

    fixed rubocop style in modified HW02

--- HW02/siarhei_pratasevich/hw02.rb
+++ HW02/siarhei_pratasevich/hw02.rb
@@ -4,31 +4,46 @@ module MyArrayMethods
   refine Array do
     def my_map(&block)
 <<<<<<< HEAD
+<<<<<<< HEAD
       return nil if block.nil?
 
 =======
       return nil if block == nil
 >>>>>>> fixed nil bug in HW02
+=======
+      return nil if block.nil?
+
+>>>>>>> fixed rubocop style in modified HW02
       reduce([]) { |result, array_element| result.push(yield(array_element)) }
     end
 
     def my_select(&block)
 <<<<<<< HEAD
-      return nil if block.nil?
-
-=======
-      return nil if block == nil
->>>>>>> fixed nil bug in HW02
-      reduce([]) { |result, array_element| yield(array_element) ? result.push(array_element) : result }
-    end
-
-    def my_each(&block)
 <<<<<<< HEAD
       return nil if block.nil?
 
 =======
       return nil if block == nil
 >>>>>>> fixed nil bug in HW02
+=======
+      return nil if block.nil?
+
+>>>>>>> fixed rubocop style in modified HW02
+      reduce([]) { |result, array_element| yield(array_element) ? result.push(array_element) : result }
+    end
+
+    def my_each(&block)
+<<<<<<< HEAD
+<<<<<<< HEAD
+      return nil if block.nil?
+
+=======
+      return nil if block == nil
+>>>>>>> fixed nil bug in HW02
+=======
+      return nil if block.nil?
+
+>>>>>>> fixed rubocop style in modified HW02
       size.times do |index|
         yield(self[index])
       end
