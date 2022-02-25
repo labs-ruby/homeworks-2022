22c03b7e68c6d1f27fb9fe527cd5e68046e4a5ce
Timemanchik <timemanchik@mail.ru>
2022-02-25 23:48:01 +0300

    fix HW2

--- HW02/siarhei_pratasevich/hw02.rb
+++ HW02/siarhei_pratasevich/hw02.rb
@@ -3,15 +3,19 @@
 module MyArrayMethods
   refine Array do
 <<<<<<< HEAD
+<<<<<<< HEAD
     def my_map(&block)
 <<<<<<< HEAD
 <<<<<<< HEAD
       return nil if block.nil?
 =======
+=======
+>>>>>>> fix HW2
     def my_map
       return enum_for(:map) unless block_given?
 >>>>>>> modified HW02 specs
 
+<<<<<<< HEAD
 =======
       return nil if block == nil
 >>>>>>> fixed nil bug in HW02
@@ -34,11 +38,19 @@ module MyArrayMethods
 =======
       return nil if block.nil?
 =======
+=======
+      reduce([]) { |result, array_element| result.push(yield(array_element)) }
+    end
+
+>>>>>>> fix HW2
     def my_select
       return enum_for(:select) unless block_given?
 >>>>>>> modified HW02 specs
 
+<<<<<<< HEAD
 >>>>>>> fixed rubocop style in modified HW02
+=======
+>>>>>>> fix HW2
       reduce([]) { |result, array_element| yield(array_element) ? result.push(array_element) : result }
     end
 
