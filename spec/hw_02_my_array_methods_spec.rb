65d91963a8c809dcdc074b7281b06af51a78b10c
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-02-28 00:48:09 +0300

    add: complete all function

--- spec/hw_02_my_array_methods_spec.rb
+++ spec/hw_02_my_array_methods_spec.rb
@@ -55,96 +55,96 @@ if File.exist?('HW02')
       end
     end
 
-    # describe '#my_map' do
-    #   context 'when block is given' do
-    #     context 'when array is empty' do
-    #       let(:array) { [] }
+    describe '#my_map' do
+      context 'when block is given' do
+        context 'when array is empty' do
+          let(:array) { [] }
 
-    #       it 'returns empty array' do
-    #         expect(array.my_map { |element| element + 1 }).to eq([])
-    #       end
-    #     end
+          it 'returns empty array' do
+            expect(array.my_map { |element| element + 1 }).to eq([])
+          end
+        end
 
-    #     context 'when array is not empty' do
-    #       let(:array) { [1, 2, 3, 4, 5] }
+        context 'when array is not empty' do
+          let(:array) { [1, 2, 3, 4, 5] }
 
-    #       it 'returns modified array' do
-    #         expect(array.my_map { |element| element + 2 }).to eq([3, 4, 5, 6, 7])
-    #       end
-    #     end
-    #   end
+          it 'returns modified array' do
+            expect(array.my_map { |element| element + 2 }).to eq([3, 4, 5, 6, 7])
+          end
+        end
+      end
 
-    #   context 'when block is not given' do
-    #     context 'when array is empty' do
-    #       let(:array) { [] }
+      context 'when block is not given' do
+        context 'when array is empty' do
+          let(:array) { [] }
 
-    #       it 'returns Enumerator object' do
-    #         expect(array.my_map).to be_kind_of(Enumerator)
-    #       end
+          it 'returns Enumerator object' do
+            expect(array.my_map).to be_kind_of(Enumerator)
+          end
 
-    #       it 'returns Enumerator/array size = 0' do
-    #         expect(array.my_each.to_a.size).to eq(0)
-    #       end
-    #     end
+          it 'returns Enumerator/array size = 0' do
+            expect(array.my_each.to_a.size).to eq(0)
+          end
+        end
 
-    #     context 'when array is not empty' do
-    #       let(:array) { [1, 2, 3, 4, 5] }
+        context 'when array is not empty' do
+          let(:array) { [1, 2, 3, 4, 5] }
 
-    #       it 'returns Enumerator object' do
-    #         expect(array.my_map).to be_kind_of(Enumerator)
-    #       end
+          it 'returns Enumerator object' do
+            expect(array.my_map).to be_kind_of(Enumerator)
+          end
 
-    #       it 'returns Enumerator/array size = 5' do
-    #         expect(array.my_map.to_a.size).to eq(5)
-    #       end
-    #     end
-    #   end
-    # end
+          it 'returns Enumerator/array size = 5' do
+            expect(array.my_map.to_a.size).to eq(5)
+          end
+        end
+      end
+    end
 
-    # describe '#my_select' do
-    #   context 'when block is given' do
-    #     context 'when array is empty' do
-    #       let(:array) { [] }
+    describe '#my_select' do
+      context 'when block is given' do
+        context 'when array is empty' do
+          let(:array) { [] }
 
-    #       it 'returns empty array' do
-    #         expect(array.my_select(&:even?)).to eq([])
-    #       end
-    #     end
+          it 'returns empty array' do
+            expect(array.my_select(&:even?)).to eq([])
+          end
+        end
 
-    #     context 'when array is not empty' do
-    #       let(:array) { [1, 2, 3, 4, 5] }
+        context 'when array is not empty' do
+          let(:array) { [1, 2, 3, 4, 5] }
 
-    #       it 'returns modified array' do
-    #         expect(array.my_select(&:even?)).to eq([2, 4])
-    #       end
-    #     end
-    #   end
+          it 'returns modified array' do
+            expect(array.my_select(&:even?)).to eq([2, 4])
+          end
+        end
+      end
 
-    #   context 'when block is not given' do
-    #     context 'when array is empty' do
-    #       let(:array) { [] }
+      context 'when block is not given' do
+        context 'when array is empty' do
+          let(:array) { [] }
 
-    #       it 'returns Enumerator object' do
-    #         expect(array.my_select).to be_kind_of(Enumerator)
-    #       end
+          it 'returns Enumerator object' do
+            expect(array.my_select).to be_kind_of(Enumerator)
+          end
 
-    #       it 'returns Enumerator/array size = 0' do
-    #         expect(array.my_select.to_a.size).to eq(0)
-    #       end
-    #     end
+          it 'returns Enumerator/array size = 0' do
+            expect(array.my_select.to_a.size).to eq(0)
+          end
+        end
 
-    #     context 'when array is not empty' do
-    #       let(:array) { [1, 2, 3, 4, 5] }
+        context 'when array is not empty' do
+          let(:array) { [1, 2, 3, 4, 5] }
 
-    #       it 'returns Enumerator object' do
-    #         expect(array.my_select).to be_kind_of(Enumerator)
-    #       end
+          it 'returns Enumerator object' do
+            expect(array.my_select).to be_kind_of(Enumerator)
+          end
 
-    #       it 'returns Enumerator/array size = 5' do
-    #         expect(array.my_select.to_a.size).to eq(5)
-    #       end
-    #     end
-    #   end
-    # end
+          it 'returns Enumerator/array size = 5' do
+            expect(array.my_select.to_a.size).to eq(5)
+          end
+        end
+      end
+    end
   end
 end
