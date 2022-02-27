a9882600b8b2568975e97ba82b304d1a0236e074
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-02-28 00:20:57 +0300

    add: complete function my_each

--- HW02/aleksei_zhuchkov/hw02.rb
+++ HW02/aleksei_zhuchkov/hw02.rb
@@ -1,9 +1,20 @@
+# frozen_string_literal: true
+
 module MyArrayMethods
   refine Array do
-      def my_map; end
-      def my_select; end
-      def my_each
-        
+    def my_map; end
+    def my_select; end
+
+    def my_each
+      each unless block_given?
+      if empty?
+        []
+      else
+        for item in self
+          yield item
+        end
+        self
       end
+    end
   end
 end
--- HW02/siarhei_pratasevich/hw02.rb
+++ HW02/siarhei_pratasevich/hw02.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
-module MyArrayMethods
-  refine Array do
-    def my_map
-      return enum_for(:map) unless block_given?
+# module MyArrayMethods
+#   refine Array do
+#     def my_map
+#       return enum_for(:map) unless block_given?
 
-      reduce([]) { |result, array_element| result.push(yield(array_element)) }
-    end
+#       reduce([]) { |result, array_element| result.push(yield(array_element)) }
+#     end
 
-    def my_select
-      return enum_for(:select) unless block_given?
+#     def my_select
+#       return enum_for(:select) unless block_given?
 
-      reduce([]) { |result, array_element| yield(array_element) ? result.push(array_element) : result }
-    end
+#       reduce([]) { |result, array_element| yield(array_element) ? result.push(array_element) : result }
+#     end
 
-    def my_each
-      return enum_for(:each) unless block_given?
+#     def my_each
+#       return enum_for(:each) unless block_given?
 
-      size.times do |index|
-        yield(self[index])
-      end
-      self
-    end
-  end
-end
+#       size.times do |index|
+#         yield(self[index])
+#       end
+#       self
+#     end
+#   end
+# end
