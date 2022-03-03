85e16912ba00fcb5a459c7e1375bdff5e56d22c8
Timemanchik <timemanchik@mail.ru>
2022-03-03 18:11:12 +0300

    rebase HW03

--- HW03/Siarhei_Pratasevich/siarhei_pratasevich_hw_03_t_02.rb
+++ HW03/Siarhei_Pratasevich/siarhei_pratasevich_hw_03_t_02.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class Homework3
-  def task2(log, result = [])
+  def task2(log, result_array = [])
     log.split("\n").each do |string|
       next unless string.scan(%r{\S+\s-\s-\s\D\d+/\w+/\S+\s\+\S+\s\S+\s\S+\s\w+/\d\.\d"\s\d+\s\S+\s\S+}).join == string
 
-      result
+      result_array
         .push("DATE: #{string.split(/[\[\]]/)[1]} FROM: #{string.split(' ')[0]} TO: #{string.split(' ')[6].upcase}")
     end
-    result
+    result_array
   end
 end
