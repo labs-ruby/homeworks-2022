ac9b5d46365be6e8babf5d8067f8a173c007e1f0
anastasia <evsihneyeva@gmail.com>
2022-03-03 23:58:32 +0300

    Changed my_each, my_map and my_select

--- HW02/anastasia_evsihneyeva/hw02.rb
+++ HW02/anastasia_evsihneyeva/hw02.rb
@@ -4,23 +4,31 @@ module MyArrayMethods
   refine Array do
     def my_map(&block)
       new_arr = []
+      return to_enum unless block_given?
+
       for i in self
-        new_arr << block.call(i) if block_given?
+        new_arr << block.call(i)
       end
-      new_arr.to_enum
+      new_arr
     end
 
     def my_select(&block)
       new_arr = []
+      return to_enum unless block_given?
+
       for i in self
-        new_arr << i if block_given? && block.call(i)
+        new_arr << i if block.call(i)
       end
-      new_arr.to_enum
+      new_arr
     end
 
     def my_each
-      for i in self
-        yield i if block_given?
+      if block_given?
+        for i in self
+          yield i
+        end
+      else
+        to_enum
       end
     end
   end
