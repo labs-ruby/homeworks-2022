bfe33fd83b343718743147dc480ce086a4b4ed17
VitalyKadyrko <55240701+VitalyKadyrko@users.noreply.github.com>
2022-03-04 10:15:22 +0300

    Changed implementation of #my_each method

---
+++ HW02/hw02.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# app/models/user.rb
+module MyArrayMethods
+  refine Array do
+    def my_select
+      result_array = []
+      if block_given?
+        size.times do |i|
+          result_array << self[i] if yield(self[i])
+        end
+        result_array
+      else
+        enum_for
+      end
+    end
+
+    def my_map
+      result_array = []
+      if block_given?
+        size.times do |i|
+          result_array << yield(self[i])
+        end
+        result_array
+      else
+        enum_for
+      end
+    end
+
+    def my_each
+      if block_given?
+        size.times do |i|
+          yield(self[i])
+        end
+        self
+      else
+        enum_for 
+      end
+    end
+  end
+end
