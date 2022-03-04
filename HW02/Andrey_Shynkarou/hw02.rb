70300d4947e564ce32186d42f1cf671e19634fc4
mgst-d <and8511@ya.ru>
2022-03-04 23:01:56 +0300

    Add my_map

--- HW02/Andrey_Shynkarou/hw02.rb
+++ HW02/Andrey_Shynkarou/hw02.rb
@@ -1,7 +1,19 @@
+# frozen_string_literal: true
 module MyArrayMethods
   refine Array do
     def my_map
+      if block_given?
+        self.size.times do |i|
+          self[i] = yield(self[i])
+        end
+        return self
+
+      else
+        return self.to_enum
+      end
+      
     end
+
     def my_select
     end
     def my_each
