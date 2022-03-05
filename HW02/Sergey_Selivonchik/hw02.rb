6c4473a39f7acd338af971cfc1001be651043bb1
Ваше Имя <elhom99@gmail.com>
2022-03-05 08:47:02 +0300

    hw_02_v2.0

--- HW02/Sergey_Selivonchik/hw02.rb
+++ HW02/Sergey_Selivonchik/hw02.rb
@@ -3,27 +3,38 @@
 module MyArrayMethods
   refine Array do
     def my_map
-      mapped_array = []
-      length.times do |i|
-        mapped_array.push yield self[i]
+      if block_given?
+        mapped_array = []
+        length.times do |i|
+          mapped_array.push yield self[i]
+        end
+        mapped_array
+      else
+        to_enum
       end
-      mapped_array
     end
 
     def my_select
-      mapped_array = []
-      length.times do |i|
-        mapped_array.push self[i] if (yield self[i]) == true
+      if block_given?
+        mapped_array = []
+        length.times do |i|
+          mapped_array.push self[i] if yield self[i]
+        end
+        mapped_array
+      else
+        to_enum
       end
-      mapped_array
     end
 
     def my_each
-      mapped_array = []
-      length.times do |i|
-        mapped_array.push self[i]
+      if block_given?
+        length.times do |i|
+          yield self[i]
+        end
+        self
+      else
+        to_enum
       end
-      mapped_array
     end
   end
 end
