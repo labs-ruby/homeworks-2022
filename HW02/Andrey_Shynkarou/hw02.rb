76d7a0d8d8215d8f7da731f750b325e74c14df7e
mgst-d <and8511@ya.ru>
2022-03-05 13:05:56 +0300

    Correct with rubocop

--- HW02/Andrey_Shynkarou/hw02.rb
+++ HW02/Andrey_Shynkarou/hw02.rb
@@ -1,38 +1,32 @@
 # frozen_string_literal: true
+
 module MyArrayMethods
   refine Array do
     def my_map
       if block_given?
-        self.size.times do |i|
-          self[i] = yield(self[i])
-        end
-        return self
-
+        size.times { |i| self[i] = yield(self[i]) }
+        self
       else
-        return self.to_enum
+        to_enum
       end
-      
     end
 
     def my_select
       if block_given?
         a = []
-        self.size.times do |i|
-          a << self[i] if yield(self[i])
-        end
-        return a
+        size.times { |i| a << self[i] if yield(self[i]) }
+        a
       else
-        return self.to_enum
+        to_enum
       end
     end
+
     def my_each
       if block_given?
-        self.size.times do |i|
-          yield(self[i])
-        end
-        return self
+        size.times { |i| yield(self[i]) }
+        self
       else
-        return self.to_enum
+        to_enum
       end
     end
   end
