a84275b54b16342c8d33b231ccc6b9b9068d5e7d
Dmitry <ddmitiy@mail.ru>
2022-03-06 03:44:29 +0300

    Correct method map

--- HW02/dmitry_permiakov/hw02.rb
+++ HW02/dmitry_permiakov/hw02.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 module MyArrayMethods
-     refine Array do
-         def my_map
-         return to_enum unless block_given?
-         total = []
-         for i in self {total << i}
-         return total
-         
-         def my_select
-         return to_enum unless block_given?
-         
-         end
-         def my_each
-         return to_enum unless block_given?
-         
-         
-         end
-     end
- end
+  refine Array do
+    def my_map
+      return to_enum unless block_given?
+
+      total = []
+      for i in self
+        total << i
+      end
+      total
+    end
+
+    def my_select
+      return to_enum unless block_given?
+    end
+
+    def my_each
+      return to_enum unless block_given?
+    end
+  end
+end
