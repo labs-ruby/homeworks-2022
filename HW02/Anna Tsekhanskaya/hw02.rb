febbd14ca837b071e00f3d8265836c31bfe3442c
tsekhanskaya <76792113+tsekhanskaya@users.noreply.github.com>
2022-03-07 11:17:56 +0300

    Deleted extra spaces and lines in hw02.rb

--- HW02/Anna Tsekhanskaya/hw02.rb
+++ HW02/Anna Tsekhanskaya/hw02.rb
@@ -1,39 +1,39 @@
+#frozen_string_literal: true
+
 module MyArrayMethods
-    refine Array do
-
-        def my_map
-            result_array = []
-            if block_given?
-                for elem in self 
-                    result_array << block.call(elem)
-                end
-                result_array
-            else
-                to_enum
-            end
+  refine Array do
+    def my_map
+      result_array = []
+      if block_given?
+        for elem in self
+          result_array << block.call(elem)
         end
+        result_array
+       else
+         to_enum
+       end
+     end
 
-        def my_select
-            result_array = []
-            if block_given?
-                for elem in self 
-                    result_array << elem if block.call(elem)
-                end
-                result_array
-            else
-                to_enum
-            end
+    def my_select
+      result_array = []
+      if block_given?
+        for elem in self
+          result_array << elem if block.call(elem)
         end
-
-        def my_each
-            if block_given?
-                for elem in self 
-                    yeild (elem)
-                end
-            else
-                to_enum
-            end
+        result_array
+      else
+        to_enum
+      end
+     end
+    
+    def my_each
+      if block_given?
+        for elem in self
+          yeild(elem)
         end
-
+      else
+        to_enum
+      end
     end
+  end
 end
