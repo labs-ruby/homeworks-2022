54df4f8e02b75b3ecbfbf946f8114d9bc4acb153
tsekhanskaya <76792113+tsekhanskaya@users.noreply.github.com>
2022-03-07 09:44:33 +0300

    Add HW02 Tsekhanskaya

---
+++ HW02/Anna Tsekhanskaya/hw02.rb
@@ -0,0 +1,39 @@
+module MyArrayMethods
+    refine Array do
+
+        def my_map
+            result_array = []
+            if block_given?
+                for elem in self 
+                    result_array << block.call(elem)
+                end
+                result_array
+            else
+                to_enum
+            end
+        end
+
+        def my_select
+            result_array = []
+            if block_given?
+                for elem in self 
+                    result_array << elem if block.call(elem)
+                end
+                result_array
+            else
+                to_enum
+            end
+        end
+
+        def my_each
+            if block_given?
+                for elem in self 
+                    yeild (elem)
+                end
+            else
+                to_enum
+            end
+        end
+
+    end
+end
