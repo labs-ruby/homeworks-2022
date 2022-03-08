4794b77ac579f9a7c1d380bc0d9e0b461f7fb98d
manzhaimaksim <maxbbb88@gmail.com>
2022-03-08 14:12:20 +0300

    creating task1

--- HW03/maksim_manzhai/maksim_manzhai_hw_03_t_01.rb
+++ HW03/maksim_manzhai/maksim_manzhai_hw_03_t_01.rb
@@ -1,1 +1,15 @@
-class Homework3; end
+# frozen_string_literal: true
+
+class Homework3
+  def task1(logs)
+    if logs.empty?
+      ''
+    else
+      lines = logs.split(/\n/)
+      lines.each do |line|
+        return line if line.include?('error')
+      end
+    end
+    ''
+  end
+end
