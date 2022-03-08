dd4d9b3b5ab4b1ce294fa3f7053dc2f95560eba5
manzhaimaksim <maxbbb88@gmail.com>
2022-03-08 19:25:25 +0300

    create task2

--- HW03/maksim_manzhai/maksim_manzhai_hw_03_t_02.rb
+++ HW03/maksim_manzhai/maksim_manzhai_hw_03_t_02.rb
@@ -1,1 +1,29 @@
-class Homework3; end
+# frozen_string_literal: true
+
+class Homework3
+  def task2(logs)
+    return [] if logs.empty?
+
+    lines = logs.split(/\n/)
+    lines.each do |line|
+      return [] unless right_format?(line)
+    end
+    format_lines(lines)
+  end
+
+  private
+
+  def right_format?(string)
+    string.match(/^\w/) ? true : false
+  end
+
+  def format_lines(strings)
+    temp_array = []
+    strings.each do |string|
+      temp_array << ("DATE: " + string.scan(%r{\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2}\s\+\d{4}}).join + 
+        " FROM: " + string.scan(%r{\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}}).join +
+        " TO: " + string.scan(%r{\/\w*\/\d*\/\w*}).join.upcase) unless string.include?('error')
+    end
+    temp_array
+  end
+end
