4c122dfcb3a16c134c9713bb03e30d597de3841f
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-03-09 01:39:01 +0300

    add: complete task4 in HM03

--- HW03/aleksei_zhuchkov/aleksei_zhuchkov_hw_03_t_04.rb
+++ HW03/aleksei_zhuchkov/aleksei_zhuchkov_hw_03_t_04.rb
@@ -1,1 +1,11 @@
+# frozen_string_literal: true
 
+class Homework3
+  def task4(data_log = 'error')
+    raise ArgumentError if data_log == 'error'
+
+    count_digits = data_log.scan(/\d/).flatten.count
+    count_letters = data_log.scan(/[a-zA-Z]/).flatten.count
+    { letters: count_letters, digits: count_digits }
+  end
+end
