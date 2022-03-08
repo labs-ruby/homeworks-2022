dfaee1f9d82604347e262b733783ebd22c8c959c
Darya Zhalova <5432595@gmail.com>
2022-03-08 23:13:14 +0300

    rubocop changes

--- HW03/Darya_Zhalova/darya_zhalova_hw_03_t_01.rb
+++ HW03/Darya_Zhalova/darya_zhalova_hw_03_t_01.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 class Homework3
   def task_1(input)
-    for line in input.lines do
-      if line.include? "error"
-        return line
-      end
+    input.lines.each do
+      return line if line.include? 'error'
     end
-    ""
+    ''
   end
 end
