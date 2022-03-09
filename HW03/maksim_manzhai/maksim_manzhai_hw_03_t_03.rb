41b9a78fd27ef7652455482fe5c27f1b97c945f4
manzhaimaksim <maxbbb88@gmail.com>
2022-03-09 20:22:43 +0300

    creating task3

--- HW03/maksim_manzhai/maksim_manzhai_hw_03_t_03.rb
+++ HW03/maksim_manzhai/maksim_manzhai_hw_03_t_03.rb
@@ -1,1 +1,34 @@
-class Homework3; end
+# frozen_string_literal: true
+
+require 'time'
+
+class Homework3
+  def task3(log)
+    lines = log.split(/\n/)
+    temp_array = []
+    lines.each do |line|
+      temp_array << Time.parse(line) if event_valid?(line)
+    end
+    return '0' if temp_array.empty?
+
+    return '0' if temp_array.size == 1
+
+    duration(temp_array)
+  end
+
+  private
+
+  def event_valid?(string)
+    string.include?('Calling core with action') ? true : false
+  end
+
+  def duration(events)
+    temp_array = []
+    (0...events.size).each do |i|
+      temp_array << (events[i + 1] - events[i]).to_s unless events[i + 1].nil?
+    end
+    return temp_array[0] if temp_array.size == 1
+
+    temp_array
+  end
+end
