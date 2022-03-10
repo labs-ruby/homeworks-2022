80d4005d55afd7802d0d7273492ca70d94342fd5
TiBulat <bulat.n.tim@gmail.com>
2022-03-10 09:55:26 +0300

    change regex

--- HW03/tim_bulat/tim_bulat_hw_03_t_02.rb
+++ HW03/tim_bulat/tim_bulat_hw_03_t_02.rb
@@ -2,6 +2,8 @@
 
 class ClassName
   IP_REGEX = /\b(?:\d{1,3}\.){3}\d{1,3}\b/
+  DATE_REGEX = /\[.*?\]/
+  TO_REGEX = /".*?"/
 
   def get_lines(log, key_word)
     log.split("\n").select { |line| line != '' && line.include?(key_word) }
@@ -10,9 +12,9 @@ class ClassName
   def task2(log)
     err_log = get_lines(log, 'POST')
     err_log.map do |line|
-      "DATE: #{line[/\[.*?\]/].delete('[]')} "\
+      "DATE: #{line[DATE_REGEX].delete('[]')} "\
       "FROM: #{line[IP_REGEX]} "\
-      "TO: #{line[/".*?"/].delete!('POST').split[1].upcase}"
+      "TO: #{line[TO_REGEX].delete!('POST').split[1].upcase}"
     end
   end
 end
