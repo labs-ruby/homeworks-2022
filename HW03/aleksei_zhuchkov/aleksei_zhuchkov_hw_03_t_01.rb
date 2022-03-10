fb17449697f0debd8eec44892f1ef48650fb9fa6
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-03-10 23:47:39 +0300

    refactor: complete refactor task 01 HW03

--- HW03/aleksei_zhuchkov/aleksei_zhuchkov_hw_03_t_01.rb
+++ HW03/aleksei_zhuchkov/aleksei_zhuchkov_hw_03_t_01.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
-
 class Homework3
   def task1(data_log)
-    array_data_log = data_log.split("\n")
-    array_data_error = array_data_log.select { |el| el.to_s.downcase.include?('error') }
-    array_data_error[0] || ''
+    data_log.split(/\n/).find{ |el| el.to_s.downcase.include?('error') } || ""
   end
 end
