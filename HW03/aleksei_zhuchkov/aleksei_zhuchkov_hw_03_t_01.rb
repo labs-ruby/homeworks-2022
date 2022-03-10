52e718cad67136c6bb398f417690ccfed267c993
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-03-11 00:42:56 +0300

    refactor: was done refactor task 02 HW03

--- HW03/aleksei_zhuchkov/aleksei_zhuchkov_hw_03_t_01.rb
+++ HW03/aleksei_zhuchkov/aleksei_zhuchkov_hw_03_t_01.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
+
 class Homework3
   def task1(data_log)
-    data_log.split(/\n/).find{ |el| el.to_s.downcase.include?('error') } || ""
+    data_log.split(/\n/).find { |el| el.to_s.downcase.include?('error') } || ''
   end
 end
--- HW03/aleksei_zhuchkov/aleksei_zhuchkov_hw_03_t_02.rb
+++ HW03/aleksei_zhuchkov/aleksei_zhuchkov_hw_03_t_02.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 class Homework3
-  def task2(data_log = 'error')
-    result = []
-    raise ArgumentError if data_log == 'error'
+  IP_REGEX = /^\S+/
+  DATE_REGEX = /(?<=\[).+?(?=\])/
+  HEAD_REGEX = /(?<=POST )\S+/
 
-    data_log.split("\n").each do |el|
-      ip = el.match(/^\S+/) || next
-      date = el.match(/(?<=\[).+?(?=\])/) || next
-      head = el.match(/POST (\S+)/) || next
-      result.push("DATE: #{date} FROM: #{ip} TO: #{head[1].upcase}")
+  def task2(data_log)
+    result = []
+    raise ArgumentError unless data_log.instance_of?(String)
+
+    data_log.split(/\n/).each do |el|
+      ip = el.match(IP_REGEX) || next
+      date = el.match(DATE_REGEX) || next
+      head = el.match(HEAD_REGEX).to_s.upcase || next
+      result.push("DATE: #{date} FROM: #{ip} TO: #{head}")
     end
     result
   end
