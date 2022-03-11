fd50dfb39d31e7160a05537445f1d6e77b66745e
Yuliya <yuliyaxsheremet@gmail.com>
2022-03-12 01:30:31 +0300

    Second commit

--- HW03/yuliya_sheremet/yuliya_sheremet_hw03_task3.rb
+++ HW03/yuliya_sheremet/yuliya_sheremet_hw03_task3.rb
@@ -25,12 +25,3 @@ class Homework3
     end
   end
 end
-homework3 = Homework3.new
-homework3.task3("2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
-2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
-2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
-2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
-2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
-2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
-2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
-")
