1c7461d597e28362e9061877b77d504d84c1622a
anastasia <evsihneyeva@gmail.com>
2022-03-11 21:25:48 +0300

    Second part of the 3rd exersise

--- HW03/anastasia_evsihneyeva/anastasia_evsihneyeva_hw_03_t_02.rb
+++ HW03/anastasia_evsihneyeva/anastasia_evsihneyeva_hw_03_t_02.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
 class Homework3
-  def task2; end
+  DATE = /\[(.*)\]/
+  FROM = /(.*)(?=\ - -)/
+  TO = %r{/te(.*)(?=\ H)}
+  def task2(logs)
+    return [] if logs.nil?
+
+    variable = logs.to_s.split("\n")
+    info = []
+    variable.each do |log|
+      next unless log.include?('POST') && log =~ /(.*) - - \[(.*)\] "(.*)" (.*)/
+
+      str = "DATE: #{DATE.match(log)[1]} FROM: #{FROM.match(log)} TO: #{TO.match(log).to_s.upcase}"
+      info << str
+    end
+    info
+  end
 end
