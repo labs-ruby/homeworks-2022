baea75b02fb7e2b4fab1cf257e7ad23e9bc04739
mgst-d <and8511@ya.ru>
2022-03-11 16:10:40 +0300

    Refactoring task1

--- HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_01.rb
+++ HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_01.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 class Homework3
-
+  def initialize
+    @ERROR = 'error'
+  end
   def task1(log)
-    answer = ''
-    log.each_line do |s|
-      if s.include?("error")
-        answer = s.chomp
-        break
-      end
-    end
-    return answer
+    log.lines
+            .select { |s| s.include?(@ERROR) }
+            .first.to_s.chomp
   end
 end
