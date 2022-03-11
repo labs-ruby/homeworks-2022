78b7bb2a9529dd4b69a1e69879951869a1181e51
mgst-d <and8511@ya.ru>
2022-03-11 13:49:36 +0300

    Add task1

--- HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_01.rb
+++ HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_01.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 class Homework3
-  def task1
-    # your implementation goes here
+
+  def task1(log)
+    answer = ''
+    log.each_line do |s|
+      if s.include?("error")
+        answer = s.chomp
+        break
+      end
+    end
+    return answer
   end
 end
