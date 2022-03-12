09885a67800449fbbaa830050728e379a9e00461
anastasia <evsihneyeva@gmail.com>
2022-03-12 20:12:47 +0300

    Fixed hird part of the 3rd exersise again

--- HW03/anastasia_evsihneyeva/anastasia_evsihneyeva_hw_03_t_03.rb
+++ HW03/anastasia_evsihneyeva/anastasia_evsihneyeva_hw_03_t_03.rb
@@ -1,25 +1,34 @@
 # frozen_string_literal: true
 
 require 'time'
-require 'set'
 
 class Homework3
   NUMS = /(.*)(?=\ ub)/
   SPLIT = /[\ :-]/
   def task3(logs)
-    logs = logs.split("\n").select { |log| log.include?('core with action') }
-    return '0' if logs.length < 2
+    return '0' if finding_logs(logs).length < 2
 
-    times = []
+    logs = finding_dur(making_time(finding_logs(logs)))
+
+    logs.length == 1 ? logs[0] : logs
+  end
+
+  private
+
+  def finding_logs(logs)
+    logs.split("\n").select { |log| log.include?('core with action') }
+  end
+
+  def making_time(logs)
+    logs.map { |log| Time.gm(*NUMS.match(log).to_s.split(SPLIT).map(&:to_f)) }
+  end
+
+  def finding_dur(logs)
     results = []
-    logs.each do |log|
-      times << Time.gm(*NUMS.match(log).to_s.split(SPLIT).map(&:to_f))
-    end
 
-    (0..times.length - 2).each do |i|
-      results << (times[i + 1] - times[i]).round(1).to_s
+    (0..logs.length - 2).each do |i|
+      results << (logs[i + 1] - logs[i]).round(1).to_s
     end
-
-    results.length == 1 ? results[0] : results
+    results
   end
 end
