366c5a38db6cf846c2e060ab46a483502e90cf81
anastasia <evsihneyeva@gmail.com>
2022-03-12 11:57:41 +0300

    Forth part of the 3rd exersise

--- HW03/anastasia_evsihneyeva/anastasia_evsihneyeva_hw_03_t_04.rb
+++ HW03/anastasia_evsihneyeva/anastasia_evsihneyeva_hw_03_t_04.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class Homework3
-  def task4; end
+  def task4(string)
+    result = { letters: 0, digits: 0 }
+    string.split('').each do |i|
+      result[:letters] += 1 if i.match(/^[[:alpha:]]$/)
+      result[:digits] += 1 if i.match(/^[[:digit:]]$/)
+    end
+    result
+  end
 end
