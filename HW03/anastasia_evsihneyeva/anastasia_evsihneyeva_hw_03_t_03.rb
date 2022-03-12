7e589dbf94bf6596349415587a2bd76dbf6fae55
anastasia <evsihneyeva@gmail.com>
2022-03-12 19:28:54 +0300

    Fixed hird part of the 3rd exersise

--- HW03/anastasia_evsihneyeva/anastasia_evsihneyeva_hw_03_t_03.rb
+++ HW03/anastasia_evsihneyeva/anastasia_evsihneyeva_hw_03_t_03.rb
@@ -23,13 +23,3 @@ class Homework3
     results.length == 1 ? results[0] : results
   end
 end
-
-logs = "2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
-2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
-2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
-2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
-2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
-2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
-2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event"
-
-puts Homework3.new.task3(logs)
