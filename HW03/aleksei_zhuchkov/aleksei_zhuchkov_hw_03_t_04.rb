dafca72603fd958d058fbc5630fb9b7684cd8556
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-03-12 21:45:05 +0300

    refactor: refactor task 04

--- HW03/aleksei_zhuchkov/aleksei_zhuchkov_hw_03_t_04.rb
+++ HW03/aleksei_zhuchkov/aleksei_zhuchkov_hw_03_t_04.rb
@@ -4,8 +4,8 @@ class Homework3
   def task4(data_log)
     raise ArgumentError unless data_log.instance_of?(String)
 
-    count_digits = data_log.count '0-9'
-    count_letters = data_log.downcase.count 'a-z'
+    count_digits = data_log.count('0-9')
+    count_letters = data_log.count('a-zA-Z')
     { letters: count_letters, digits: count_digits }
   end
 end
