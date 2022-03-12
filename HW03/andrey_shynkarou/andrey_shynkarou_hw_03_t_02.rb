1532982e32c00bfcb258cfcfdfe96a7543cdbd8a
mgst-d <and8511@ya.ru>
2022-03-12 18:50:58 +0300

    Add task2

--- HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_02.rb
+++ HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_02.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 class Homework3
-  def task2
-    # your implementation goes here
+
+  def task2(log)
+    log.lines
+            .select { |s| s.include?('POST') }
+            .to_a
+            .map { |s| ("DATE: " + s[/(?<=\[)(.*)(?=\])/] + " FROM: " + s[/.{12}/] + " TO: " + s[/(?<=POST ).*?(?= )/].upcase) }
   end
 end
+# .map { |s| ("DATE:" + s[/\[.{26}/] + " FROM: " + s[/.{12}/]) + " TO: " + s[/POST .*?(?= )/].upcase }[\[].*[\]] [\[]+
