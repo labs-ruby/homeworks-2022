ec5b85b9d8a1bda5fd714db42ec343cab9324bb8
Darya Zhalova <5432595@gmail.com>
2022-03-12 19:57:23 +0300

    added task2 realization

--- HW03/Darya_Zhalova/darya_zhalova_hw_03_t_02.rb
+++ HW03/Darya_Zhalova/darya_zhalova_hw_03_t_02.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Homework3
+  REG_DATE = %r{\d{2}/\w+/20\d{2}:\d{2}:\d{2}:\d{2} \+\d{4}}
+  REG_ADR = /\d{2}\.\d\.\d{3}\.\d{3}/
+  REG_PATH = %r{/test/2/\w+}
+
   def task2(log)
+    log.lines.map do |line|
+      "DATE: #{line[REG_DATE]} FROM: #{line[REG_ADR]} TO: #{line[REG_PATH].upcase}" unless line.include? 'ERROR'
+    end.compact
   end
 end
