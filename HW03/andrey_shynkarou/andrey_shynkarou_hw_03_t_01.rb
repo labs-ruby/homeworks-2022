5a3fd8afdab9489b4f43da0ad49881617ce46abc
mgst-d <and8511@ya.ru>
2022-03-13 15:36:21 +0300

    Add task4, correct task1

--- HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_01.rb
+++ HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_01.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class Homework3
-  def initialize
-    @ERROR = 'error'
-  end
+
   def task1(log)
     log.lines
-            .select { |s| s.include?(@ERROR) }
+            .select { |s| s.include?('error') }
             .first.to_s.chomp
   end
 end
--- HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_04.rb
+++ HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_04.rb
@@ -1,5 +1,10 @@
 class Homework3
-  def task4
-    # your implementation goes here
+  def task4(s)
+    h = {letters: 0, digits: 0}
+    s.each_char do |item|
+      h[:letters] = h[:letters] + 1 if item.match?(/[a-zA-Z]/)
+      h[:digits] = h[:digits] + 1 if item.match?(/[\d]/)
+    end
+    h
   end
 end
