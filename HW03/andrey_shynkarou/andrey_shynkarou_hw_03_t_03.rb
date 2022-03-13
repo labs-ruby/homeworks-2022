444b17337f82d8448479687a41ff748e8ae3ca0d
mgst-d <and8511@ya.ru>
2022-03-13 15:10:27 +0300

    Add task3

--- HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_03.rb
+++ HW03/andrey_shynkarou/andrey_shynkarou_hw_03_t_03.rb
@@ -1,5 +1,22 @@
+require 'time'
 class Homework3
-  def task3
-    # your implementation goes here
+  def task3(log)
+
+    def event_time(log)
+      log.lines
+              .select { |s| s.include?('action')}
+              .map { |s| s[/\d{4}\-\d{2}\-\d{2} \d{2}\:\d{2}\:\d{2}\.\d/] }
+              .map { |s| Time.parse(s) }
+    end
+    def subtract_time(log_time)
+      a = log_time.each_with_index.map { |x, i| (log_time[i+1]-x).to_s if (i < (log_time.length - 1)) }
+                                  .select.with_index { |x, i| i != (log_time.length-1) }
+
+      return a.first if a.length == 1
+      a if a.length != 1
+    end
+
+    event_time(log).length <= 1 ? "0" : subtract_time(event_time(log))
+
   end
 end
