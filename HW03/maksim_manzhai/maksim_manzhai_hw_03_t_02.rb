a4f2904a61ec00571a9989b0418227ba9f4cf2c6
manzhaimaksim <maxbbb88@gmail.com>
2022-03-13 19:11:42 +0300

    rewrite task2

--- HW03/maksim_manzhai/maksim_manzhai_hw_03_t_02.rb
+++ HW03/maksim_manzhai/maksim_manzhai_hw_03_t_02.rb
@@ -1,45 +1,25 @@
 # frozen_string_literal: true
 
-CONVERT_DATE = %r{\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2}\s\+\d{4}}
-CONVERT_IP = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
-CONVERT_PATH = %r{/\w*/\d*/\w*}
+DATETIME = %r{\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2}\s\+\d{4}}
+IP = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
+ADDRESS = %r{/\w*/\d*/\w*}
 
 class Homework3
   def task2(logs)
     return [] if logs.empty?
 
     lines = logs.split(/\n/)
-    lines.each do |line|
-      return [] unless right_format?(line)
+
+    formated_lines = lines.find_all { |line| line.include?('POST') }
+    formated_lines.each { |line| return [] if wrong_format?(line) }
+    formated_lines.collect do |line|
+      "DATE: #{line.scan(DATETIME).join} FROM: #{line.scan(IP).join} TO: #{line.scan(ADDRESS).join.upcase}"
     end
-    format_lines(lines)
   end
 
   private
 
-  def right_format?(string)
-    string.match(/^\w/) ? true : false
-  end
-
-  def format_lines(strings)
-    temp_array = []
-    strings.each do |string|
-      next if string.include?('error')
-
-      temp_array << "DATE: #{convert_date(string)} FROM: #{convert_ip(string)} TO: #{convert_path(string)}"
-    end
-    temp_array
-  end
-
-  def convert_date(string)
-    string.scan(CONVERT_DATE).join
-  end
-
-  def convert_ip(string)
-    string.scan(CONVERT_IP).join
-  end
-
-  def convert_path(string)
-    string.scan(CONVERT_PATH).join.upcase
+  def wrong_format?(string)
+    string.match(/^\W/) ? true : false
   end
 end
