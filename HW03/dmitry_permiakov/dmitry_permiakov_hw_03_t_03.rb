bfd7b5d05437a0c69502020281c3502a8420e289
Dmitry <ddmitiy@mail.ru>
2022-03-14 00:16:19 +0300

    Solution of the 3 task

---
+++ HW03/dmitry_permiakov/dmitry_permiakov_hw_03_t_03.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'time'
+
+class Homework3
+  def task3(logs)
+    arr = logs.split("\n").select { |v| v.include? 'Calling core with action' }.map { |x| Time.parse(x) }
+
+    return '0' if arr.length < 2
+
+    arr.each_cons(2).map { |x, y| (x - y).abs.to_s }
+  end
+end
