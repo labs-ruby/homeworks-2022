68002114dc70934252fe5c46f13faa367b3909f9
Timemanchik <timemanchik@mail.ru>
2022-03-13 08:16:50 +0300

    HW04 before rubocop -A

---
+++ HW04/siarhei_pratasevich/lib/homework.rb
@@ -0,0 +1,9 @@
+class Homework
+  attr_reader :title, :description, :student
+
+  def initialize(title:, description:, student:)
+    @title = title
+    @description = description
+    @student = student
+  end
+end
