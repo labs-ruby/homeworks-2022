09a9107cac49d533de5b935ec23c293c59bfe2e0
agodes84 <renegad84@gmail.com>
2022-03-13 22:09:52 +0300

    task1 done

--- HW03/andrei_godes/andrei_godes_hw_03_t_01.rb
+++ HW03/andrei_godes/andrei_godes_hw_03_t_01.rb
@@ -2,12 +2,7 @@
 
 class Homework3
   def task1(log)
-    arr = log.split("\n").map do |text|
-      if text.include?('error') then text
-      else
-        ' '
-      end
-    end
-    arr.each { |element| puts element }
+    arr = log.split("\n").map {|text| text.include?('error')}.to_s
+    
   end
 end
