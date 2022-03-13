eaa37c122928458051da8729aba25a9697ef670a
agodes84 <renegad84@gmail.com>
2022-03-13 23:15:23 +0300

    task4_correct

--- HW03/andrei_godes/andrei_godes_hw_03_t_04.rb
+++ HW03/andrei_godes/andrei_godes_hw_03_t_04.rb
@@ -2,8 +2,6 @@
 
 class Homework3
   def task4(logs)
-    let = logs.count('A-z')
-    dig = logs.count('0-9')
-    "digits: #{dig}, letters: #{let}"
+    { letters: logs.count(A - z), digits: logs.count('0-9') }
   end
 end
