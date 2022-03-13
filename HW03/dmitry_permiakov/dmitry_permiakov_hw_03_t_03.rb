4c999b8188ecacdf811e40cdb0d41260464240d3
Dmitry <ddmitiy@mail.ru>
2022-03-14 00:44:47 +0300

    Coorect task3 , difference array

--- HW03/dmitry_permiakov/dmitry_permiakov_hw_03_t_03.rb
+++ HW03/dmitry_permiakov/dmitry_permiakov_hw_03_t_03.rb
@@ -10,6 +10,6 @@ class Homework3
 
     time_difference = arr.each_cons(2).map { |x, y| (x - y).abs.to_s }
 
-    arr.length == 1 ? time_difference[0] : time_difference
+    arr.length == 2 ? time_difference[0] : time_difference
   end
 end
