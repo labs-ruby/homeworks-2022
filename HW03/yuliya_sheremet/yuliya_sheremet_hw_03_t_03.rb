1fa74a885a3cbb67454edc026be674a98df54d68
Julittaes <jayesonly@gmail.com>
2022-03-13 23:13:09 +0300

    Fixed all issues in 3rd task

--- HW03/yuliya_sheremet/yuliya_sheremet_hw_03_t_03.rb
+++ HW03/yuliya_sheremet/yuliya_sheremet_hw_03_t_03.rb
@@ -1,7 +1,6 @@
 # HW03/task3.rb
 # frozen_string_literal: true
 
-require 'rubocop'
 require 'date'
 require 'time'
 
@@ -13,37 +12,31 @@ class Homework3
 
   def cases_for_output(logs, array_of_strings)
     if logs.scan('Calling core').length > 1
-      if_include(array_of_strings)
+      include_calling_core?(array_of_strings)
       seconds = array_of_strings
-      check_and_print(seconds)
+      print(seconds)
     else
       '0'
     end
   end
 
-  def print_a(seconds)
-    i = 0
-    (Time.parse(seconds[i + 1]) - Time.parse(seconds[i])).to_s
+  def one_value(seconds)
+    (Time.parse(seconds[1]) - Time.parse(seconds[0])).to_s
   end
 
-  def print_b(seconds)
+  def two_values(seconds)
     arr = [] << (Time.parse(seconds[1]) - Time.parse(seconds[0])).to_s
     arr << (Time.parse(seconds[2]) - Time.parse(seconds[1])).to_s
   end
 
-  def check_and_print(seconds)
-    if seconds.size <= 2
-      print_a(seconds)
-    elsif seconds.size > 2
-      print_b(seconds)
-    end
+  def print(seconds)
+    seconds.size <= 2 ? one_value(seconds) : two_values(seconds)
   end
 
-  def if_include(array_of_strings)
+  def include_calling_core?(array_of_strings)
     array_of_strings.select! { |line| line.include? 'Calling core' }
-    array_of_strings.map! do |p|
-      data = p.split(' ')
-      [data[0], data[1]].join(' ')
+                    .map do |line|
+      line.split(' ')[0..1].join(' ')
     end
   end
 end
