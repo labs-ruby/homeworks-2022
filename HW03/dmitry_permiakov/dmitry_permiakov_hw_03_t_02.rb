d3ef05e4bc4d8a8688548d6eaa3464e197876be6
Dmitry <ddmitiy@mail.ru>
2022-03-14 00:53:25 +0300

    Check Date on Empty

--- HW03/dmitry_permiakov/dmitry_permiakov_hw_03_t_02.rb
+++ HW03/dmitry_permiakov/dmitry_permiakov_hw_03_t_02.rb
@@ -4,7 +4,7 @@ class Homework3
   def task2(logs)
     logs.split("\n").select { |v| v.include? 'POST' }.map do |x|
       x = x.split(' ')
-      return [] if x[3][1..] == Empty
+      return [] if x[3][1..] == ''
 
       "DATE: #{x[3][1..]} #{x[4][0..-2]} FROM: #{x[0]} TO: #{x[6].upcase}"
     end
