776ed6acbf501762c4e3cc994b11e2d7de66278e
TimeForward <roman.nasyrov@gmail.com>
2022-03-14 12:36:42 +0300

    Update roman_nasyrov_hw_03_t_01.rb
    update to resolve offenses

--- HW03/roman_nasyrov/roman_nasyrov_hw_03_t_01.rb
+++ HW03/roman_nasyrov/roman_nasyrov_hw_03_t_01.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
-
-
 class Homework3
-  def task1(input)
-    input.lines.each do |line|
-      return line.chomp if line.include? 'error'
+    def task1(input)
+      input.lines.each do |line|
+        return line.chomp if line.include? 'error'
+      end
+  
     end
-    
   end
-end
