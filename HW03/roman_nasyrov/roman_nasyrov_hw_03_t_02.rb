f6f514e2d1a8f4c8df7d128ae3051f013387d5af
TimeForward <roman.nasyrov@gmail.com>
2022-03-14 13:21:54 +0300

    Update roman_nasyrov_hw_03_t_02.rb

--- HW03/roman_nasyrov/roman_nasyrov_hw_03_t_02.rb
+++ HW03/roman_nasyrov/roman_nasyrov_hw_03_t_02.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
+
+# declaring class
 class Homework3
-    REGEX = %r{^([\d+.\d+]+).*(?<=\[)(.*)(?=\]).*(?<=\s)((?:/\w+)*?)(?=\s).*$}
-    def task2(log)
-      log.split("\n").each_with_object([]) do |verse, memo|
-          verse.scan(REGEX) do |ip, dt, fold|
-          memo << "DATE: #{dt} FROM: #{ip} TO: #{fold.upcase}"
-        end
+  REGEX = %r{^([\d+.\d+]+).*(?<=\[)(.*)(?=\]).*(?<=\s)((?:/\w+)*?)(?=\s).*$}.freeze
+  def task2(log)
+    log.split("\n").each_with_object([]) do |verse, memo|
+      verse.scan(REGEX) do |ip, dt, fold|
+        memo << "DATE: #{dt} FROM: #{ip} TO: #{fold.upcase}"
       end
     end
   end
+end
