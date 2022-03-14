0157c43a69553224b7f3185dff7c4306171cf105
VitalyKadyrko <55240701+VitalyKadyrko@users.noreply.github.com>
2022-03-14 03:36:45 +0300

    update task_2

--- HW03/vitaly_kadyrko/vitaly_kadyrko_hw_03_t_02.rb
+++ HW03/vitaly_kadyrko/vitaly_kadyrko_hw_03_t_02.rb
@@ -4,10 +4,9 @@ class Homework3
   def task2(log)
     output_array = []
     good_data_array = log.split("\n").select { |x| x.length in 65..100 }
-    [] if good_data_array.size.zero?
     good_data_array.each do |x|
-      x = x.split
-      output_array << "DATE: #{x[3][1..]} #{x[4][0..4]} FROM: #{x[0]} TO: #{x[6].upcase}"
+      el = x.split
+      output_array << "DATE: #{el[3][1..]} #{el[4][0..4]} FROM: #{el[0]} TO: #{el[6].upcase}"
     end
     output_array
   end
