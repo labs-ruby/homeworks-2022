3dff413b574c9baa2487993a99fc98b5e4887737
tsekhanskaya <anutta2222@gmail.com>
2022-03-14 11:22:47 +0300

    Added anna_tsekhanskaya_hw_03_t_02.rb with task2

---
+++ HW03/anna_tsekhanskaya/anna_tsekhanskaya_hw_03_t_02.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+# anna_tsekhanskaya_hw_03_t_02.rb
+
+class Homework3
+  FORM = %r{\d{2}\.\d\.\d{3}\.\d{3}\s-\s-\s\[\S+\s\+\d{4}\]\s"POST\s/\S+\s\S+"\s\d+\s+\S+\s+\S+}
+  DATE = /.*?\[([^)]*)\].*/
+  TO = /.*?POST ([^)]*)\ HTTP.*/
+  IP = /\d{2}\.\d\.\d{3}\.\d{3}/
+  def task2(logs)
+    logs.split("\n").map do |line|
+      return [] unless line ~= FORM
+      "DATE: #{line.match(DATE)[1]} FROM: #{line.match(IP)[0]} TO: #{line.match(TO)[1].upcase}"
+    end
+  end
+end
