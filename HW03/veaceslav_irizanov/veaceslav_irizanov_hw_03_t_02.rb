b90086322a33fa2bc76f26ac8ac976804d27831d
Slavahaim <slava.irizanov@gmail.com>
2022-03-14 14:16:07 +0300

    Made changes as per Boris comment -task2

--- HW03/veaceslav_irizanov/veaceslav_irizanov_hw_03_t_02.rb
+++ HW03/veaceslav_irizanov/veaceslav_irizanov_hw_03_t_02.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class Homework3
-  CONDITION_ERROR = 'ERROR'
   CONDITION = %r{\S+\s-\s-\s\D\d+/\w+/\S+\s\+\S+\s\S+\s\S+\s\w+/\d\.\d"\s\d+\s\S+\s\S+}
   DATETIME = %r{\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2}\s\+\d{4}}
   IP = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
   ADRESS = %r{/\w*/\d*/\w*}
   def task2(log)
-    rows = log.split("\n").delete_if { |str| str.include?(CONDITION_ERROR) }
+    rows = log.split("\n").select { |str| str.include?('POST') }
     rows.map do |element|
       return [] unless element == element.scan(CONDITION).join
 
