f33152af33b0406693856d3c053ce13efcfada0f
Slavahaim <slava.irizanov@gmail.com>
2022-03-15 12:45:39 +0300

    Changed CONST-name -task2

--- HW03/veaceslav_irizanov/veaceslav_irizanov_hw_03_t_02.rb
+++ HW03/veaceslav_irizanov/veaceslav_irizanov_hw_03_t_02.rb
@@ -4,13 +4,13 @@ class Homework3
   CONDITION = %r{\S+\s-\s-\s\D\d+/\w+/\S+\s\+\S+\s\S+\s\S+\s\w+/\d\.\d"\s\d+\s\S+\s\S+}
   DATETIME = %r{\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2}\s\+\d{4}}
   IP = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
-  ADRESS = %r{/\w*/\d*/\w*}
+  ADDRESS = %r{/\w*/\d*/\w*}
   def task2(log)
     rows = log.split("\n").select { |str| str.include?('POST') }
     rows.map do |element|
       return [] unless element == element.scan(CONDITION).join
 
-      "DATE: #{element.scan(DATETIME).join} FROM: #{element.scan(IP).join} TO: #{element.scan(ADRESS).join.upcase}"
+      "DATE: #{element.scan(DATETIME).join} FROM: #{element.scan(IP).join} TO: #{element.scan(ADDRESS).join.upcase}"
     end
   end
 end
