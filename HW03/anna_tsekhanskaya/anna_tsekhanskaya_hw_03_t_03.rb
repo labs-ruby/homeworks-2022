deba966b78b94f2a47e0aa5dc5b5c5ab777f63b1
tsekhanskaya <anutta2222@gmail.com>
2022-03-16 20:15:43 +0300

    Used command 'rubocop -a'

--- HW03/anna_tsekhanskaya/anna_tsekhanskaya_hw_03_t_03.rb
+++ HW03/anna_tsekhanskaya/anna_tsekhanskaya_hw_03_t_03.rb
@@ -7,12 +7,12 @@ require 'time'
 class Homework3
   def task3(logs)
     return splited_transformed(logs)[0] if splited_transformed(logs.size == 1)
-    return "0" if logs.map {|log| Time.parse(log)}.size <=1    
+    return '0' if logs.map { |log| Time.parse(log) }.size <= 1
   end
 
   def splited_transformed(logs)
-    logs.split("\n").select{|log| log.include?("Calling core with action:'")}
-      .map{|log| Time.parse(log)}
-      .each_cons(2).map! { |time1, time2| (time1 - time2).abs.to_s })
+    logs.split("\n").select { |log| log.include?("Calling core with action:'") }
+        .map { |log| Time.parse(log) }
+        .each_cons(2).map! { |time1, time2| (time1 - time2).abs.to_s }
   end
 end
