225ffa57d1507346a77c1ec092930bb5c8c71772
Igor Shkuryn <34629950+Shkuryn@users.noreply.github.com>
2022-03-17 11:36:45 +0300

    Update HW03/ihar_shkuryn/ihar_shkuryn_hw_03_t_03.rb
    replaced with a one line statement
    
    Co-authored-by: Boris Tsarikov <36640907+B0RlS@users.noreply.github.com>

--- HW03/ihar_shkuryn/ihar_shkuryn_hw_03_t_03.rb
+++ HW03/ihar_shkuryn/ihar_shkuryn_hw_03_t_03.rb
@@ -5,8 +5,7 @@ require 'time'
 class Homework3
   PATTERN = /Calling core with action:/
   def task3(log)
-    array_lines = log.split("\n")
-    durations = array_lines.select { |x| x.match(PATTERN) }
+    durations = log.split("\n").select { |line| line.match(PATTERN) }
                            .map { |line| Time.parse(line) }
                            .each_cons(2).map { |a, b| (b - a).to_s }
     if durations.size > 1
