35d3022d8cee005ac25d2d40e5d9130c391634cb
manzhaimaksim <maxbbb88@gmail.com>
2022-03-18 21:00:55 +0300

    adding subcribe for Mentor Class

--- HW04/maksim_manzhai/lib/mentor.rb
+++ HW04/maksim_manzhai/lib/mentor.rb
@@ -1,15 +1,24 @@
 # frozen_string_literal: true
 
 class Mentor
+  attr_accessor :subscriptions, :notifications
+
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @surname = attributes.fetch(:surname)
+    @subscriptions = []
+    @notifications = []
   end
 
   def add_homework(attributes)
     @title = attributes.fetch(:title)
     @description = attributes.fetch(:description)
     @student = attributes.fetch(:student)
-    Homework.new(title: @title, description: @description, student: @student)
+    @mentor = attributes.fetch(:mentor)
+    Homework.new(title: @title, description: @description, student: @student, mentor: @mentor)
+  end
+
+  def subscribe_to!(student)
+    @subscriptions << student
   end
 end
