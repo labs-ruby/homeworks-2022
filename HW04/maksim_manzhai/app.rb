e12dcf69dbe69829e2e0bc2dbb33de06c0bd4c97
manzhaimaksim <maxbbb88@gmail.com>
2022-03-18 22:19:12 +0300

    uncomment lines in interaction file

--- HW04/maksim_manzhai/app.rb
+++ HW04/maksim_manzhai/app.rb
@@ -13,34 +13,34 @@ homework = mentor.add_homework(title: 'HW03', description: 'OOP in Ruby', studen
 student.notifications
 # student mark as read all notifications
 student.mark_as_read!
-# # mentor subscribe to student
+# mentor subscribe to student
 mentor.subscribe_to!(student)
-# # student take to work homework
+# student take to work homework
 student.homeworks
 student.to_work!(homework)
 student.homeworks
-# # mentor see notification about homework to work
+# mentor see notification about homework to work
 mentor.notifications
-# # mentor mark as read all notifications
-# mentor.mark_as_read!
-# mentor.notifications
-# # student can see homeworks
-# student.homeworks
-# # student add answer to homework
-# student.add_answer!(homework, 'new students answer')
-# # student sent to check homework
+# mentor mark as read all notifications
+mentor.mark_as_read!
+mentor.notifications
+# student can see homeworks
+student.homeworks
+# student add answer to homework
+student.add_answer!(homework, 'new students answer')
+# student sent to check homework
+student.to_check!(homework)
+# mentor see notification about homework to check
+mentor.notifications
+# mentor reject homework
+mentor.reject_to_work!(homework)
+# student see notification about homework was reject
+student.notifications
+# ---
+student.add_answer!(homework, 'new students answer_after reject')
 # student.to_check!(homework)
-# # mentor see notification about homework to check
-# mentor.notifications
-# # mentor reject homework
-# mentor.reject_to_work!(homework)
-# # student see notification about homework was reject
-# student.notifications
-# # ---
-# student.add_answer!(homework, 'new students answer_after reject')
-# student.to_check!(homework)
-# # ---
-# # mentor accept homework
+# ---
+# mentor accept homework
 # mentor.accept!(homework)
-# # student see notification about homework was accept
+# student see notification about homework was accept
 # student.notifications
