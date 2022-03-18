553fdee260fa2db715d136663e4d00be55012c42
manzhaimaksim <maxbbb88@gmail.com>
2022-03-18 22:18:00 +0300

    adding methods for Mentor class

--- HW04/maksim_manzhai/lib/mentor.rb
+++ HW04/maksim_manzhai/lib/mentor.rb
@@ -21,4 +21,12 @@ class Mentor
   def subscribe_to!(student)
     @subscriptions << student
   end
+
+  def mark_as_read!
+    @notifications.each { |notification| notification.readed = true }
+  end
+
+  def reject_to_work!(homework)
+    homework.student.notifications << Notification.new(title: homework.title, description: 'Your homework rejected')
+  end
 end
