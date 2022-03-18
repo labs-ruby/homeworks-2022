d2e96ecc8042ede9bd4ecf3b95367cdda20757fc
manzhaimaksim <maxbbb88@gmail.com>
2022-03-18 21:01:36 +0300

    adding method to_work! for Student class

--- HW04/maksim_manzhai/lib/student.rb
+++ HW04/maksim_manzhai/lib/student.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 class Student
-  attr_accessor :notifications
+  attr_accessor :notifications, :homeworks
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @surname = attributes.fetch(:surname)
     @notifications = []
+    @homeworks = []
   end
 
   def mark_as_read!
     @notifications.each { |notification| notification.readed = true }
+  end
+
+  def to_work!(homework)
+    @homeworks << homework
+    homework.mentor.notifications << Notification.new(title: homework.title, description: 'Homework started')
   end
 end
