5696e37b2a9de73335111d0dcf597a8d31b0f136
manzhaimaksim <maxbbb88@gmail.com>
2022-03-18 20:59:58 +0300

    adding attribute mentor for Homework class

--- HW04/maksim_manzhai/lib/homework.rb
+++ HW04/maksim_manzhai/lib/homework.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class Homework
+  attr_reader :title
+  attr_accessor :mentor
+
   def initialize(attributes)
     @title = attributes.fetch(:title)
     @description = attributes.fetch(:description)
     @student = attributes.fetch(:student)
+    @mentor = attributes.fetch(:mentor)
     @student.notifications << Notification.new(title: @title, description: @description)
   end
 end
