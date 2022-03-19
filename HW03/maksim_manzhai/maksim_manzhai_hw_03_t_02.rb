e009570e04943e4945807f246017cfa2cb3d2e72
Manzhai Maksim <62567109+manzhaimaksim@users.noreply.github.com>
2022-03-19 19:41:22 +0300

    My hw 03 (#64)

---
+++ HW03/maksim_manzhai/maksim_manzhai_hw_03_t_02.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+DATETIME = %r{\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2}\s\+\d{4}}
+IP = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
+ADDRESS = %r{/\w*/\d*/\w*}
+
+class Homework3
+  def task2(logs)
+    lines = logs.split(/\n/)
+
+    return [] if lines.find { |line| wrong_format?(line) }
+
+    lines.find_all { |line| line.include?('POST') }.collect do |line|
+      "DATE: #{line.scan(DATETIME).join} FROM: #{line.scan(IP).join} TO: #{line.scan(ADDRESS).join.upcase}"
+    end
+  end
+
+  private
+
+  def wrong_format?(line)
+    line.match(/^\W/)
+  end
+end
