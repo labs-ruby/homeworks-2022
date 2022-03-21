23ad4d0ac59bced0d9ad6f9cf673477806221614
Slavahaim <slava.irizanov@gmail.com>
2022-03-21 14:52:44 +0300

    Added class Member & empty def

--- HW04/veaceslav_irizanov/lib/mentor.rb
+++ HW04/veaceslav_irizanov/lib/mentor.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
-class Mentor
+class Mentor < Member
+  def notifications; end
+
+  def add_homework(title:, description:, student:); end
+
+  def subscribe_to!(student); end
+
+  def mark_as_read!; end
+
+  def reject_to_work!(homework); end
+
+  def accept!(homework); end
 end
--- HW04/veaceslav_irizanov/lib/student.rb
+++ HW04/veaceslav_irizanov/lib/student.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
-class Student
+require_relative '../lib/notification'
+require_relative '../lib/member'
+
+class Student < Member
+  def notifications; end
+
+  def mark_as_read!; end
+
+  def homeworks; end
+
+  def to_works!(homework); end
+
+  def add_answer!(homework, 'new students answer'); end
+
+  def to_check!(homework); end
 end
