9132f380676f68a221d2223ecc88fddbfdbaa252
Igor <shkuryn@gmail.com>
2022-03-21 22:53:17 +0300

    mentor add homework

--- HW04/ihar_shkuryn/lib/mentor.rb
+++ HW04/ihar_shkuryn/lib/mentor.rb
@@ -13,36 +13,11 @@ class Mentor
     @homeworks_for_check = []
   end
 
-  def notifications
-    # puts 'notice to mentor'
-  end
-
   def add_homework(data)
     homework = Homework.new(data, self)
+    debugger
     all_homeworks.push(homework)
     data[:student].homeworks_todo.push(homework)
     homework
-  end
-
-  def mark_as_read!
-    notice.mark_as_read!
-    # puts 'notice has been read by mentor'
-  end
-
-  def accept!(homework)
-    homeworks_for_check.delete(homework)
-    homework.student.homeworks_todo.delete(homework)
-    homework.student.notice.queue.push("Mentor #{name} accepted homework #{homework.title}")
-  end
-
-  def reject_to_work!(homework)
-    homework.student.homeworks_todo.push(homework)
-    homeworks_for_check.delete(homework)
-    homework.student.notice.queue.push("Mentor #{name} rejected homework #{homework.title}")
-  end
-
-  def subscribe_to!(student)
-    @subscriptions.push(student)
-    student.notice.queue.push("Mentor #{@name} subscribed to #{student.name}")
   end
 end
