6377908357b01f10c9ad93c274daf18147bd0fe1
tsekhanskaya <anutta2222@gmail.com>
2022-03-22 16:54:48 +0300

    Change a little task3

--- HW03/anna_tsekhanskaya/anna_tsekhanskaya_hw_03_t_03.rb
+++ HW03/anna_tsekhanskaya/anna_tsekhanskaya_hw_03_t_03.rb
@@ -5,21 +5,21 @@
 require 'time'
 
 class Homework3
-  TIMESTAMP = /^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d/.freeze
+  TIMESTAMP = /^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d/
   def task3(logs)
     return '0' if events(logs).length < 2
+
     difference(events)
-    
   end
 
   def events(logs)
     log = logs.split("\n")
-    events = log.select{ |log| log.include?("Calling core with action") }.map
+    events = log.select { |log| log.include?('Calling core with action') }.map
   end
 
   def difference(logs)
     diff = []
     diff << logs.each_cons(2).map! { |previos_event, next_event| (next_event - previos_event).to_s }
-    diff.join(",")
+    diff.join(',')
   end
 end
