afdae62913273c8917f93079eda1b7a5b39dd88c
tsekhanskaya <anutta2222@gmail.com>
2022-03-22 16:45:25 +0300

    Rewrite task3

--- HW03/anna_tsekhanskaya/anna_tsekhanskaya_hw_03_t_03.rb
+++ HW03/anna_tsekhanskaya/anna_tsekhanskaya_hw_03_t_03.rb
@@ -5,14 +5,21 @@
 require 'time'
 
 class Homework3
+  TIMESTAMP = /^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d/.freeze
   def task3(logs)
-    return splited_transformed(logs)[0] if splited_transformed(logs.size == 1)
-    return '0' if logs.map { |log| Time.parse(log) }.size <= 1
+    return '0' if events(logs).length < 2
+    difference(events)
+    
   end
 
-  def splited_transformed(logs)
-    logs.split("\n").select { |log| log.include?("Calling core with action:'") }
-        .map { |log| Time.parse(log) }
-        .each_cons(2).map! { |time1, time2| (time1 - time2).abs.to_s }
+  def events(logs)
+    log = logs.split("\n")
+    events = log.select{ |log| log.include?("Calling core with action") }.map
+  end
+
+  def difference(logs)
+    diff = []
+    diff << logs.each_cons(2).map! { |previos_event, next_event| (next_event - previos_event).to_s }
+    diff.join(,)
   end
 end
