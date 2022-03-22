943647ad1e117a1fb58ab34f2e072f6a04928773
matveibos <matveibos@mail.ru>
2022-03-22 23:17:32 +0300

    add homework

--- HW04/matsvei_basiayeu/lib/homework.rb
+++ HW04/matsvei_basiayeu/lib/homework.rb
@@ -1,1 +1,20 @@
+# frozen_string_literal: true
 
+class Homework
+  attr_reader :title
+  attr_accessor :mentor, :answers, :student, :status
+
+  def initialize(title:, description:, student:, mentor:, status: 'unchecked')
+    @title = title
+    @description = description
+    @student = student
+    @answers = []
+    @mentor = mentor
+    @status = status
+    send_notify
+  end
+
+  def send_notify
+    @student.notifications << Notification.new(title: @title, description: @description)
+  end
+end
