07618948418aca0585ed58225fe60636b8a27bbd
Ivan <klimovs2008van@gmail.com>
2022-03-23 10:10:08 +0300

    refactor mentor.rb

--- HW04/ivan_klimov/lib/mentor.rb
+++ HW04/ivan_klimov/lib/mentor.rb
@@ -2,27 +2,30 @@
 
 class Mentor < Person
   def add_homework(title:, description:, student:)
-    input_hw(student_homeworks(student), add_hw(description))
-    notification_for_mentor(self, title)
-    # notification_for_student(student,notification(title))
-    Homework.new(title: title, description: description, student: student)
+    hw = Homework.new(title: title, description: description, student: student)
+    student.homeworks << hw.description
+    student.notifications << add_hw(title)
+    hw
   end
 
   def subscribe_to!(student)
-    notification_for_student(student, notification("#{name} #{surname} subscribe to you!"))
+    student.notifications.clear if nf_is_empty(student)
+    student.notifications << subscribe_to(self)
   end
 
   def mark_as_read!
-    notification_for_mentor(self, read_all)
+    notifications.clear
+    notifications << read_all(self)
   end
 
   def reject_to_work!(student, homework)
-    notification_for_student(student,
-                             notification("#{name} #{surname} answer for #{homework.title} is wrong."))
+    student.notifications.clear if nf_is_empty(student)
+    student.notifications << hw_reject(homework)
   end
 
   def accept!(student, homework)
-    notification_for_student(student,
-                             notification("#{name} #{surname} answer for #{homework.title} is right!"))
+    student.notifications.clear if nf_is_empty(student)
+    student.notifications.delete(hw_reject(homework))
+    student.notifications << hw_accept(homework)
   end
 end
