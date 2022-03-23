751b53cb437e77104694bb5b042292d8653c4555
Ivan <klimovs2008van@gmail.com>
2022-03-23 10:14:07 +0300

    refactor homework.rb

--- HW04/ivan_klimov/lib/homework.rb
+++ HW04/ivan_klimov/lib/homework.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Homework
-  attr_reader :title, :description, :student
+  attr_reader :title, :description, :student, :answers
 
   def initialize(title:, description:, student:)
     @title = title
     @description = description
     @student = student
+    @answers = []
   end
 end
