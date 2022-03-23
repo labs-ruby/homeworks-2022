3da01caabc2fee0c30639a6187ea1ec5ceb75214
manzhaimaksim <maxbbb88@gmail.com>
2022-03-23 21:15:40 +0300

    editing Homework class

--- HW04/maksim_manzhai/lib/homework.rb
+++ HW04/maksim_manzhai/lib/homework.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 class Homework
-  attr_reader :title
-  attr_accessor :mentor, :answers, :student, :acceptable
+  attr_reader :title, :mentor, :student
+  attr_accessor :answers, :acceptable
 
   def initialize(title:, description:, student:, mentor:)
     @title = title
     @description = description
     @student = student
     @mentor = mentor
-    send_notification
+    send_notification_to_student
     @answers = []
     @acceptable = false
   end
 
-  def send_notification
+  def send_notification_to_student
     @student.notifications << Notification.new(title: @title, description: @description)
   end
 end
