5c1c8d030681bbf673b392ffcef4add627fe9961
Ivan <klimovs2008van@gmail.com>
2022-03-23 10:04:06 +0300

    refactor app.rb

--- HW04/ivan_klimov/app.rb
+++ HW04/ivan_klimov/app.rb
@@ -4,7 +4,6 @@ require_relative 'lib/student'
 require_relative 'lib/mentor'
 require_relative 'lib/homework'
 require_relative 'lib/notification'
-require_relative 'lib/file_module'
 
 student = Student.new(name: 'Ivan', surname: 'Klimov')
 mentor = Mentor.new(name: 'Olga', surname: 'Urban')
@@ -15,33 +14,37 @@ homework = mentor.add_homework(title: 'task1',
                                student: student)
 
 # student see notification about new homework
-student.notifications(homework)
+p student.notifications
 # student mark as read all notifications
-student.mark_as_read!(mentor)
+student.mark_as_read!
 # mentor subscribe to student
 mentor.subscribe_to!(student)
 # student take to work homework
-student.homeworks
+p student.homeworks
 student.to_work!(mentor, homework)
-student.homeworks
+p student.homeworks
 # mentor see notification about homework to work
+p mentor.notifications
 # mentor mark as read all notifications
 mentor.mark_as_read!
+p mentor.notifications
 # student can see homeworks
-student.homeworks
+p student.homeworks
 # student add answer to homework
-student.add_answer!(homework, 'p (2+3*6).')
+student.add_answer!(homework, ' 2+3*2 ')
 # student sent to check homework
 student.to_check!(mentor, homework)
 # mentor see notification about homework to check
+p mentor.notifications
 # mentor reject homework
 mentor.reject_to_work!(student, homework)
 # student see notification about homework was reject
+p student.notifications
 # ---
-student.add_answer!(homework, 'p eval(2+3*6).')
+student.add_answer!(homework, 'eval(2+3*2')
 student.to_check!(mentor, homework)
 # ---
 # mentor accept homework
 mentor.accept!(student, homework)
 # student see notification about homework was accept
-# student.notifications
+p student.notifications
