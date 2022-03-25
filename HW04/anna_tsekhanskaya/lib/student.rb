2fe9878cd15433a8cb5dcf9351d148eb2f7ee08c
tsekhanskaya <anutta2222@gmail.com>
2022-03-25 14:22:12 +0300

    Change nitifications in student.rb

--- HW04/anna_tsekhanskaya/lib/student.rb
+++ HW04/anna_tsekhanskaya/lib/student.rb
@@ -3,12 +3,14 @@
 require 'time'
 
 class Student
-  atter_accessor :name, :surname, :noties_for_mentor
+  attr_accessor :name, :surname, :notifications
+  attr_reader :mentor
+
   def initialize
     @name = name
     @surname = surname
     @answer = []
-    @noties_for_mentor = []
+    @notifications = []
     @list_homework = []
   end
 
@@ -19,13 +21,13 @@ class Student
   # student see notification about new homework
   # student see notification about homework was reject
   # student see notification about homework was accept
-  def notifications
-    noties_for_mentor.clear
-  end
+  # def notifications
+  # notifications.clear
+  # end
 
   # student mark as read all notifications
   def mark_as_read
-    noties_for_mentor.clear
+    notifications.clear
     Notification.mark_as_read = true
   end
 
@@ -37,22 +39,22 @@ class Student
 
   # student take to work homework
   def to_work(homework)
-    noties_for_mentor << Notification.new(title: 'Working',
-                                          description: "Student #{Student.full_name} starts working at #{Time.now}")
+    notifications << Notification.new(title: 'Working',
+                                      description: "Student #{Student.full_name} starts working at #{Time.now}")
     list_homework << Homework.new(title: '', description: answer, student: Student.full_name, mentor: Mentor.full_name)
     answer << homework
   end
 
   # student add answer to homework
   def add_answer(homework, str)
-    noties_for_mentor << Notification.new(title: str,
-                                          description: "Student #{Student.full_name} added answer at #{Time.now}")
+    notifications << Notification.new(title: str,
+                                      description: "Student #{Student.full_name} added answer at #{Time.now}")
     answer << homework
   end
 
   # student sent to check homework
   def to_check
-    noties_for_mentor << Notification.new(title: 'Add answer',
-                                          description: "Student #{Student.full_name} sent hw to check #{Time.now}")
+    mentor.notifications << Notification.new(title: 'Add answer',
+                                             description: "Student #{Student.full_name} sent hw to check #{Time.now}")
   end
 end
