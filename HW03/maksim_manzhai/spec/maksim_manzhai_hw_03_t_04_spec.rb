7ca42889749f2fde70496ee2c07fe70613843e51
manzhaimaksim <maxbbb88@gmail.com>
2022-04-02 17:24:18 +0300

    adding tests for task4

---
+++ HW03/maksim_manzhai/spec/maksim_manzhai_hw_03_t_04_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require_relative '../maksim_manzhai_hw_03_t_04'
+
+RSpec.describe Homework3 do
+  describe '#task4' do
+    before { @homework = described_class.new }
+
+    context 'when the string contains only letters' do
+      let(:input) do
+        'qwerty'
+      end
+
+      let(:output) { { digits: 0, letters: 6 } }
+
+      it 'returns the count of letters and digits in that string' do
+        expect(@homework.task4(input)).to eql output
+      end
+
+      it 'returns an object of class Hash' do
+        expect(@homework.task4(input)).to be_a(Hash)
+      end
+    end
+
+    context 'when the string contains only digits' do
+      let(:input) do
+        '123456'
+      end
+
+      let(:output) { { digits: 6, letters: 0 } }
+
+      it 'returns the count of letters and digits in that string' do
+        expect(@homework.task4(input)).to eql output
+      end
+
+      it 'returns an object of class Hash' do
+        expect(@homework.task4(input)).to be_a(Hash)
+      end
+    end
+
+    context 'when the string contains digits and letters' do
+      let(:input) do
+        'qwerty123456'
+      end
+
+      let(:output) { { digits: 6, letters: 6 } }
+
+      it 'returns the count of letters and digits in that string' do
+        expect(@homework.task4(input)).to eql output
+      end
+
+      it 'returns an object of class Hash' do
+        expect(@homework.task4(input)).to be_a(Hash)
+      end
+    end
+  end
+end
