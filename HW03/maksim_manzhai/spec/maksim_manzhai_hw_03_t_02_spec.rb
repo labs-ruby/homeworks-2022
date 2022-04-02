03f2c7aad93bdca3916118b45618dfb60282727d
manzhaimaksim <maxbbb88@gmail.com>
2022-04-02 14:06:39 +0300

    adding test to check log for wrong strings

--- HW03/maksim_manzhai/spec/maksim_manzhai_hw_03_t_02_spec.rb
+++ HW03/maksim_manzhai/spec/maksim_manzhai_hw_03_t_02_spec.rb
@@ -32,5 +32,22 @@ RSpec.describe Homework3 do
         expect(@homework.task2(log)).to be_a(Array)
       end
     end
+
+    context 'when log does contain wrong strings' do
+      let(:log) do
+        <<~LOG
+           10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+          10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
+          2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
+          10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290
+        LOG
+      end
+
+      let(:output) { [] }
+
+      it 'return empty array' do
+        expect(@homework.task2(log)).to eql output
+      end
+    end
   end
 end
