c863e681c554acbb2d77db038cbb068f47fe6971
Igor <shkuryn@gmail.com>
2022-04-03 20:57:10 +0300

    add simple tests for hw03_04

--- HW05/HW03/ihar_shkuryn_hw_03_t_04.rb
+++ HW05/HW03/ihar_shkuryn_hw_03_t_04.rb
@@ -3,6 +3,8 @@
 # frozen
 class Homework3
   def task4(log)
+    raise ArgumentError, 'Only string are allowed' unless log.is_a?(String)
+
     letters_count = log.count('a-zA-Z')
     digits_count = log.count('0-9')
     { letters: letters_count, digits: digits_count }
---
+++ HW05/spec/homework3_task_04_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative '../HW03/ihar_shkuryn_hw_03_t_04'
+
+RSpec.describe Homework3 do
+  let(:obj) { described_class.new }
+
+  context 'when log is a nil' do
+    let(:log) { nil }
+
+    it 'raised argument error' do
+      expect { obj.task4(log) }.to raise_error(ArgumentError, 'Only string are allowed')
+    end
+  end
+
+  context 'when word is empty' do
+    it 'returns number of letters=0 and digits=0' do
+      expect(obj.task4('  ')).to eq({ digits: 0, letters: 0 })
+    end
+  end
+
+  context 'when word is special symbols' do
+    it 'returns number of letters=0 and digits=0' do
+      expect(obj.task4('!_+-)(')).to eq({ digits: 0, letters: 0 })
+    end
+  end
+
+  context 'when got array in params' do
+    let(:log) { ['error'] }
+
+    it 'raised argument error' do
+      expect { obj.task4(log) }.to raise_error(ArgumentError, 'Only string are allowed')
+    end
+  end
+
+  context 'when boolean in params' do
+    let(:log) { true }
+
+    it 'raised argument error' do
+      expect { obj.task4(log) }.to raise_error(ArgumentError, 'Only string are allowed')
+    end
+  end
+end
