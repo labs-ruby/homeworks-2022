64f1c192cdc9f17e733ee424d668250d8aa0ca41
Igor <shkuryn@gmail.com>
2022-04-05 19:54:19 +0300

    add first tests for mentor

---
+++ HW05/spec/mentor_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative '../HW04/lib/mentor'
+require_relative 'spec_helper'
+
+RSpec.describe Mentor do
+  let(:name) { 'Jack' }
+  let(:surname) { 'Gonsales' }
+  let(:obj) { described_class.new(name: name, surname: surname) }
+
+  describe 'checking fields of instance' do
+    context 'when fields is correct' do
+      it 'returns instance of Student' do
+        expect(obj).to be_an_instance_of(described_class)
+      end
+
+      it 'when name is correct' do
+        expect(obj.name).to eq(name)
+      end
+
+      it 'when surname is correct' do
+        expect(obj.surname).to eq(surname)
+      end
+
+      it 'when returns correct presentation' do
+        expect(obj.presentation).to eq("#{name} #{surname}")
+      end
+    end
+  end
+end
