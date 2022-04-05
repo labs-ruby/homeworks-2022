a63efdd4fb7088972b55a0a845f2e43790716d08
manzhaimaksim <maxbbb88@gmail.com>
2022-04-05 21:30:13 +0300

    adding test for Student class

--- HW04/maksim_manzhai/spec/student_spec.rb
+++ HW04/maksim_manzhai/spec/student_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'student'
+require 'notification'
 
 RSpec.describe Student do
   let(:student) { described_class.new(name: 'Chris', surname: 'Rock') }
@@ -12,6 +13,21 @@ RSpec.describe Student do
 
     it 'returns surname' do
       expect(student.surname).to eql 'Rock'
+    end
+  end
+
+  context 'when student see notification' do
+    let(:notification) { Notification.new(title: 'HW03', description: 'OOP in Ruby') }
+
+    it 'returns notifications' do
+      student.notifications << notification
+      expect(student.notifications).to eql [notification]
+    end
+
+    it 'student mark as read all notifications' do
+      student.notifications << notification
+      student.mark_as_read!
+      expect(student.notifications[0].readed).to be true
     end
   end
 end
