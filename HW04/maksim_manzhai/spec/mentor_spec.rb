dd75b4fa6b9e47888e4690f0ef0d31f99d8c1753
manzhaimaksim <maxbbb88@gmail.com>
2022-04-05 21:29:57 +0300

    adding test for Mentor class

--- HW04/maksim_manzhai/spec/mentor_spec.rb
+++ HW04/maksim_manzhai/spec/mentor_spec.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 require 'mentor'
+require 'student'
+require 'homework'
 require 'notification'
 
 RSpec.describe Mentor do
   let(:mentor) { described_class.new(name: 'Will', surname: 'Smith') }
+  let(:student) { Student.new(name: 'Chris', surname: 'Rock') }
 
   context 'when mentor created' do
     it 'returns name' do
@@ -18,10 +21,15 @@ RSpec.describe Mentor do
 
   describe '#add_homework' do
     it 'returns an object of class Homework' do
-      student = instance_double('student', name: 'Chris', surname: 'Rock', notifications: [])
       homework = mentor.add_homework(title: 'HW03', description: 'OOP in Ruby', student: student)
-
       expect(homework).to be_a(Homework)
+    end
+  end
+
+  describe '#subscribe_to!' do
+    it 'returns list of mentor subscriptions' do
+      mentor.subscribe_to!(student)
+      expect(mentor.subscriptions).to eql [student]
     end
   end
 end
