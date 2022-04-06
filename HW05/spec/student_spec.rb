3490bb02aad39ecf32128eda06e2bfa7db4317c6
Igor <shkuryn@gmail.com>
2022-04-06 09:49:34 +0300

    check student notification is instance of array

--- HW05/spec/student_spec.rb
+++ HW05/spec/student_spec.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require_relative '../HW04/lib/mentor'
 require_relative '../HW04/lib/student'
+require_relative '../HW04/lib/homework'
+require_relative '../HW04/lib/notification'
 require_relative 'spec_helper'
+require 'byebug'
 
 RSpec.describe Student do
   let(:name) { 'John' }
@@ -24,6 +28,14 @@ RSpec.describe Student do
 
       it 'when returns correct presentation' do
         expect(obj.presentation).to eq("#{name} #{surname}")
+      end
+    end
+  end
+
+  describe '#notification' do
+    context 'when student get notification' do
+      it 'student notification is instance of array' do
+        expect(obj.notices).to be_an_instance_of(Array)
       end
     end
   end
