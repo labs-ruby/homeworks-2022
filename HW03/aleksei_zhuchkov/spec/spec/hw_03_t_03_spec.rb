d57c2e89ceb5d5c59684432f90398556d4f568c9
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-04-08 00:48:57 +0300

    add tests in HW03 task3

--- HW03/aleksei_zhuchkov/spec/spec/hw_03_t_03_spec.rb
+++ HW03/aleksei_zhuchkov/spec/spec/hw_03_t_03_spec.rb
@@ -14,4 +14,40 @@ RSpec.describe 'Homework3' do
       expect(obj_hw3.task3('')).to eq('0')
     end
   end
+
+  context 'when testing log with one right result' do
+    let(:log) do
+      <<~LOGS
+        2018-12-31 23:59:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+        2018-12-31 23:59:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+        2018-12-31 23:59:49.8 ubuntu-xenial[14319] Debug - docker event processed
+        2019-01-01 00:00:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
+        2019-01-01 00:00:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+        2019-01-01 00:00:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+        2019-01-01 00:00:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
+      LOGS
+    end
+    it 'return type of strind' do
+      expect(obj_hw3.task3(log)).to be_an_instance_of(String)
+    end
+  end
+
+  context 'when there is more than 2 valid event' do
+    let(:log) do
+      <<~LOGS
+        2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+        2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+        2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
+        2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
+        2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+        2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+        2018-04-23 17:19:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+        2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
+      LOGS
+    end
+
+    it 'returns value type Array' do
+      expect(obj_hw3.task3(log)).to be_an_instance_of(Array)
+    end
+  end
 end
