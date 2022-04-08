08e22a49a7e7b17acb0cc58c783f192866e88ac3
Igor <shkuryn@gmail.com>
2022-04-08 09:07:30 +0300

    fix rubocop

--- HW05/spec/homework3_task_02_spec.rb
+++ HW05/spec/homework3_task_02_spec.rb
@@ -44,7 +44,7 @@ end
 #   <<~LOGS
 #     10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
 #     10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
-#     2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
+#     2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL:
 #     10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290
 #   LOGS
 # end
