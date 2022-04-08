cbb8a8208df99f329a2527cb10616065f95c5bf5
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-04-09 00:35:47 +0300

    add specs for HW03

---
+++ HW03/sasha_korchyk/spec/sasha_korchyk_hw_03_t_01_spec.rb
@@ -0,0 +1,48 @@
+require './sasha_korchyk_hw_03_t_01'
+
+describe Homework3 do
+  
+  describe '#task1' do
+
+    context 'log with errors' do
+
+      it 'returns error from log' do
+        log =
+          '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+          10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
+          2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
+          10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290'
+
+        expect(subject.task1(log).strip).to eq('2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>')
+      end
+
+      it 'returns the first error from the log with many errors' do
+
+        log =
+          '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+          10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
+          2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
+          2018-04-23 21:33:56: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
+          10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290'
+
+        expect(subject.task1(log).strip).to eq('2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>')
+      end
+
+  end
+
+    context 'log without errors' do
+
+      it 'returns an empty string' do
+        log =
+          '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+          10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
+          10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290'
+
+        expect(subject.task1(log).strip).to eq('')
+      end
+
+    end
+
+  end
+
+end
---
+++ HW03/sasha_korchyk/spec/sasha_korchyk_hw_03_t_02_spec.rb
@@ -0,0 +1,94 @@
+require './sasha_korchyk_hw_03_t_02'
+
+describe Homework3 do
+
+  describe '#task2' do
+
+    context 'calls methods and return corrent formated array' do
+
+      let(:logs) do
+          '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
+          10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
+          2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
+          10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290'
+      end
+      
+      let(:correct_output) do
+        [
+          "DATE: 23/Apr/2018:20:30:39 +0300 FROM: 10.6.246.103 TO: /TEST/2/MESSAGES",
+          "DATE: 23/Apr/2018:20:30:42 +0300 FROM: 10.6.246.101 TO: /TEST/2/RUN",
+          "DATE: 23/Apr/2018:20:31:39 +0300 FROM: 10.6.246.101 TO: /TEST/2/MESSAGES"
+        ]
+      end
+
+      it 'calls #true_strs then #result' do
+        expect(subject).to receive(:true_strs).ordered
+        expect(subject).to receive(:result).ordered
+
+        subject.task2(logs)
+      end
+
+      it 'returns an array of formatted strings' do
+        expect(subject.task2(logs)).to eq(correct_output)
+      end
+
+    end
+
+    context 'no suitable string at the input' do
+
+      it 'returns an empty array' do
+        expect(subject.task2('')).to eq([])
+      end
+
+    end
+
+  end
+
+  describe '#true_strs' do
+
+    let(:log) do
+      '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0498
+      10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /test/2/run HTTP/1.1" 200 - 0.2277
+      2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
+      10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] "POST /test/2/messages HTTP/1.1" 200 48 0.0290'
+    end
+
+    let(:correct_output) do
+      [
+        "10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] \"POST /test/2/messages HTTP/1.1\" 200 48 0.0498",
+        "10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] \"POST /test/2/run HTTP/1.1\" 200 - 0.2277",
+        "10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] \"POST /test/2/messages HTTP/1.1\" 200 48 0.0290"
+      ] 
+    end
+
+    it 'selects the correct data from the log and returns an array with this data' do
+      expect(subject.true_strs(log)).to eq(correct_output)
+    end
+
+  end
+
+  describe '#result' do
+
+    let(:correct_data_form_log) do
+      [
+        "10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] \"POST /test/2/messages HTTP/1.1\" 200 48 0.0498",
+        "10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] \"POST /test/2/run HTTP/1.1\" 200 - 0.2277",
+        "10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] \"POST /test/2/messages HTTP/1.1\" 200 48 0.0290"
+      ]
+    end
+
+    let(:result) do
+      [
+        "DATE: 23/Apr/2018:20:30:39 +0300 FROM: 10.6.246.103 TO: /TEST/2/MESSAGES",
+        "DATE: 23/Apr/2018:20:30:42 +0300 FROM: 10.6.246.101 TO: /TEST/2/RUN",
+        "DATE: 23/Apr/2018:20:31:39 +0300 FROM: 10.6.246.101 TO: /TEST/2/MESSAGES"
+      ]
+    end
+
+      it 'formats the selected data from the log and returns an array with this data' do
+        expect(subject.result(correct_data_form_log)).to eq(result)
+      end
+
+  end
+
+end
