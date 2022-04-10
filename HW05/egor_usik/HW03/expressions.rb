23400f18ebf7fd94ff7df7fbe412f40195734203
hegoru <egorusikme@gmail.com>
2022-04-10 18:44:36 -0400

    Fixed file location

--- HW05/egor_usik/HW03/expressions.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Expressions
-  ERROR_LOG_LINE = /^(.*error).*$/
-  POST_REQUEST = /^(\d+.\d+.\d+.\d+) - - \[(.*?)\] "POST (.*) .*".*$/
-  CALLING_CORE_REQUEST = /(\d+-\d+-\d+ \d+:\d+:\d+\.\d+) ?.* - Calling core.*$/
-  LETTERS = /[A-Za-z]/
-  DIGITS = /[0-9]/
-end
