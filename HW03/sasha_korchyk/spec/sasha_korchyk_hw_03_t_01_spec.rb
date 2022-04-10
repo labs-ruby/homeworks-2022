60fb33d625bc0895f5c3ae06c0805ed937c3d0d0
Aliaksandr Korchyk <aliaksandr.korchyk@gmail.com>
2022-04-10 17:07:37 +0300

    Add spec for HW04 and rubocop fix

--- HW03/sasha_korchyk/spec/sasha_korchyk_hw_03_t_01_spec.rb
+++ HW03/sasha_korchyk/spec/sasha_korchyk_hw_03_t_01_spec.rb
@@ -1,37 +1,35 @@
+# frozen_string_literal: true
+
 require './sasha_korchyk_hw_03_t_01'
 
 describe Homework3 do
-  
   describe '#task1' do
+    context 'with log errors' do
+      let(:log_with_errors) do
+        '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+        10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
+        2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
+        2018-04-23 21:33:56: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
+        10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290'
+      end
 
-    context 'log with errors' do
-
-      it 'returns error from log' do
-        log =
-          '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
+      let(:log_with_error) do
+        '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
           10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
           2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
           10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290'
+      end
 
-        expect(subject.task1(log).strip).to eq('2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>')
+      it 'returns error from log' do
+        expect(subject.task1(log_with_error).strip).to eq('2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>')
       end
 
       it 'returns the first error from the log with many errors' do
-
-        log =
-          '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
-          10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] "POST /grid/2/event HTTP/1.1" 200 - 0.2277
-          2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
-          2018-04-23 21:33:56: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>
-          10.6.246.101 - - [23/Apr/2018:20:29:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0290'
-
-        expect(subject.task1(log).strip).to eq('2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>')
+        expect(subject.task1(log_with_errors).strip).to eq('2018-04-23 20:30:42: SSL error, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSLError: System error: Undefined error: 0 - 0>')
       end
+    end
 
-  end
-
-    context 'log without errors' do
-
+    context 'without log errors' do
       it 'returns an empty string' do
         log =
           '10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] "POST /grid/2/messages HTTP/1.1" 200 48 0.0498
@@ -40,9 +38,6 @@ describe Homework3 do
 
         expect(subject.task1(log).strip).to eq('')
       end
-
     end
-
   end
-
 end
