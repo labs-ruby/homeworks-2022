1d88043b06921cd7c99e825da06a9a5119e98a20
hegoru <59119282+hegoru@users.noreply.github.com>
2022-04-10 18:21:37 -0400

    Delete HW05/egor_usik directory

--- HW05/egor_usik/HW03/spec/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require_relative '../egor_usik_hw_03_t_01'
-require_relative '../egor_usik_hw_03_t_02'
-require_relative '../egor_usik_hw_03_t_03'
-require_relative '../egor_usik_hw_03_t_04'
