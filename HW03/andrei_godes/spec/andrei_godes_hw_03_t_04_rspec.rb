57f3ad2db9f3bd730f7e1377a8b940db234515e6
agodes84 <renegad84@gmail.com>
2022-04-10 14:12:28 +0300

    adding test for task3_4

---
+++ HW03/andrei_godes/spec/andrei_godes_hw_03_t_04_rspec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative '../andrei_godes_hw_03_t_04'
+require_relative 'spec_helper'
+
+RSpec.describe Homework3 do
+  subject { described_class.new }
+
+  context 'when string consists letters and digits' do
+    it 'returns number of letters and digits' do
+      expect(subject.task4('aefgh455fd67')).to eq({ digits: 5, letters: 7 })
+    end
+  end
+
+  context 'when string consists letters, digits and symbols' do
+    it 'returns number of letters and digits' do
+      expect(subject.task4('1654bjh78-=0kjjhv')).to eq({ digits: 7, letters: 8 })
+    end
+  end
+
+  context 'when string is empty' do
+    it 'returns number 0 letters and 0 digits' do
+      expect(subject.task4('')).to eq({ digits: 0, letters: 0 })
+    end
+  end
+end
