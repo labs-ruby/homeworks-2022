72a180dea732069193b37ed990a3ab4eb20d47df
Vitaly <wrather28@gmail.com>
2022-04-11 14:54:54 +0300

    created hw3 task4 spec

--- HW03/vitaly_kadyrko/spec/vitaly_kadyrko_hw_03_t_04_spec.rb
+++ HW03/vitaly_kadyrko/spec/vitaly_kadyrko_hw_03_t_04_spec.rb
@@ -5,5 +5,15 @@ require_relative '../vitaly_kadyrko_hw_03_t_04'
 RSpec.describe Homework3 do
   let(:t4_obj) { described_class.new }
 
-  context
+  context 'when string contains russian letter z' do
+    it 'return number of digits and letters' do
+      expect(t4_obj.task4('a2b1c6n8з')).to eq({ digits: 4, letters: 5 })
+    end
+  end
+
+  context 'when string contains special symbols' do
+    it 'return only number of digits and letters' do
+      expect(t4_obj.task4('?$a1b2c3d4++')).to eq({ digits: 4, letters: 4 })
+    end
+  end
 end
