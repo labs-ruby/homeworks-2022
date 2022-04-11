2507088762297564f5e5fda979f230127ca90ab5
Ubuntu 20.04 <an.shparun@yandex.by>
2022-04-11 06:22:13 +0300

    add spec 3

---
+++ HW03/alexey_shparun/spec/hw_03_task_03_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../alexey_shparun_hw_03_t_03'
+
+RSpec.describe Homework3 do
+  let(:obj) { described_class.new }
+
+  describe 'Log that has wrong output' do
+    context 'when testing log with one right result' do
+      let(:log) do
+        <<~LOGS
+          2018-12-31 23:59:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+          2018-12-31 23:59:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+          2018-12-31 23:59:49.8 ubuntu-xenial[14319] Debug - docker event processed
+          2019-01-01 00:00:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
+          2019-01-01 00:00:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+          2019-01-01 00:00:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+          2019-01-01 00:00:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
+        LOGS
+      end
+
+      it 'return type of strind' do
+        expect(obj.task3(log)).to be_an_instance_of(String)
+      end
+    end
+  end
+end
