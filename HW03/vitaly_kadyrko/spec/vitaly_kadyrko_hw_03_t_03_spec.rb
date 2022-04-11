510a3bfeefab8bba0d43c0507fa3713b389c2380
Vitaly <wrather28@gmail.com>
2022-04-11 14:32:20 +0300

    created task2 and task3 spec

---
+++ HW03/vitaly_kadyrko/spec/vitaly_kadyrko_hw_03_t_03_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../vitaly_kadyrko_hw_03_t_03'
+
+RSpec.describe Homework3 do
+  let(:t3_obj) { described_class.new }
+
+  context 'when one of events time is negative' do
+    let(:log) do
+      <<~LOGS
+        2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+        2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+        2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - dockmore than 2er event processed
+        2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
+        2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+        2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+        2018-04-23 17:19:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+        2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
+        2018-04-23 -17:20:18.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+      LOGS
+    end
+
+    it 'return the firs two durations of the action' do
+      expect(:t3_obj.task3(log)).to eq(['49.1', '60.0'])
+    end
+  end
+end
