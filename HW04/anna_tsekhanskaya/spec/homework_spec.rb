9b5ce59d5a0753f24879d7374f206aa721e54c6d
tsekhanskaya <anutta2222@gmail.com>
2022-04-11 04:13:03 +0300

    Create and add homework_spec.rb

--- HW04/anna_tsekhanskaya/spec/homework_spec.rb
+++ HW04/anna_tsekhanskaya/spec/homework_spec.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: true
 
-require_relative '../lib/homework'
+require_relative '/home/hanna/training/homeworks-2022/HW04/anna_tsekhanskaya/lib/homework'
+require_relative '/home/hanna/training/homeworks-2022/HW04/anna_tsekhanskaya/lib/student'
+
+RSpec.describe Homework do
+  let(:student) { Student.new(name: 'Hanna', surname: 'Zhurauskaya') }
+  homework { described_class.new(title: 'HW05', description: 'Description homework', student: student) }
+
+  describe '#title' do
+    it 'Homework has title' do
+      expect(homework.title).to eq('HW05')
+    end
+  end
+
+  describe '#description' do
+    it 'Homework has description' do
+      expect(homework.description).to eq('Description homework')
+    end
+  end
+end
