dc0cacc6aa2a4637a1ed7ed80c90e1e338413cd7
Ubuntu 20.04 <an.shparun@yandex.by>
2022-04-11 04:07:23 +0300

    add notification spec

--- HW04/alexey_shparun/spec/notification_spec.rb
+++ HW04/alexey_shparun/spec/notification_spec.rb
@@ -1,1 +1,22 @@
 # frozen_string_literal: true
+
+
+require_relative '../lib/notification'
+
+
+RSpec.describe Notification do
+
+  describe 'check notification' do
+  	context 'notification has right parametrs' do
+      it 'return notification' do
+      	expect(described_class.new('notification')).to be_instance_of(described_class)
+      end
+    end
+
+    context 'notification without the parameters' do
+      it 'returns ArgumentError' do
+        expect { described_class.new }.to raise_error(ArgumentError)
+      end
+    end
+  end
+ end
