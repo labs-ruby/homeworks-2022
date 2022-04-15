85877b45d4dfd27e5e976796f03b165e82136c73
Ivan <klimovs2008van@gmail.com>
2022-04-16 00:47:41 +0300

    refactor mentor specs

--- HW04/ivan_klimov/spec/mentor_spec.rb
+++ HW04/ivan_klimov/spec/mentor_spec.rb
@@ -10,44 +10,50 @@ require_relative '../lib/notification'
 RSpec.describe Mentor do
   let(:mentor) { described_class.new(name: 'Olga', surname: 'Urban') }
   let(:student) { Student.new(name: 'Ked', surname: '500') }
-  let(:homework) { Homework.new(title: 'task1', description: 'Solve 5/6', student: student) }
+  let(:test_homework) { mentor.add_homework(title: 'task1', description: 'Solve 5/6', student: student) }
 
   describe '#add_homework' do
+    before { mentor.add_homework(title: 'task1', description: 'Solve 5/6', student: student) }
+
     it 'returns homework' do
-      test_homework = mentor.add_homework(title: 'task1', description: 'Solve 5/6', student: student)
       expect(test_homework).to be_kind_of(Homework)
     end
 
     it 'makes notification to the student' do
-      test_homework = mentor.add_homework(title: 'task1', description: 'Solve 5/6', student: student)
-      expect(student.notifications.first.include?(test_homework.title)).to eq true
+      expect(student.notifications.first).to include(test_homework.title)
     end
   end
 
   describe '#subscribe_to!' do
-    it 'add notification to the student about subscribers' do
-      expect(mentor.subscribe_to!(student)).to eq student.notifications
+    it 'adds notification to the student about subscribers' do
+      expect { mentor.subscribe_to!(student) }.to change { student.notifications.size }.from(0).to(1)
     end
   end
 
   describe '#mark_as_read' do
-    it 'clean mentor notifications ' do
+    let(:notification) { "#{mentor.name}, you have read all notifications." }
+
+    it 'cleans mentor notifications' do
       mentor.mark_as_read!
-      expect(mentor.notifications.include?("#{mentor.name}, you have read all notifications.")).to eq true
+      expect(mentor.notifications.first).to include(notification)
     end
   end
 
   describe '#reject_to_work!' do
-    it 'add notification to the student about reject homework' do
-      mentor.reject_to_work!(student, homework)
-      expect(student.notifications.first.include?("Answer for #{homework.title}, was rejected.")).to eq true
+    let(:notification) { "Answer for #{test_homework.title}, was rejected." }
+
+    it 'adds notification to the student about reject homework' do
+      mentor.reject_to_work!(student, test_homework)
+      expect(student.notifications[1]).to include(notification)
     end
   end
 
   describe '#accept!' do
-    it 'add notification to the student about accepted homework' do
-      mentor.accept!(student, homework)
-      expect(student.notifications.first.include?("Answer for #{homework.title}, was accepted.")).to eq true
+    let(:notification) { "Answer for #{test_homework.title}, was accepted." }
+
+    it 'adds notification to the student about accepted homework' do
+      mentor.accept!(student, test_homework)
+      expect(student.notifications[1]).to include(notification)
     end
   end
 end
