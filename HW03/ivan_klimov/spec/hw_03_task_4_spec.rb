fbfa72b1341263c73fb3160aa9b453205a75fb4b
Ivan <klimovs2008van@gmail.com>
2022-04-15 23:10:10 +0300

    refactor test for the task 4 in HW03

--- HW03/ivan_klimov/spec/hw_03_task_4_spec.rb
+++ HW03/ivan_klimov/spec/hw_03_task_4_spec.rb
@@ -3,46 +3,46 @@
 require_relative '../ivan_klimov_hw_03_t_04'
 
 RSpec.describe Homework3 do
-  subject { described_class.new }
+  subject { described_class.new.task4(logs) }
 
   describe '#task4' do
-    context 'when string is empty' do
+    context 'when logs are empty' do
       let(:logs) { '' }
 
-      it 'returns hash with 0' do
-        expect(subject.task4(logs)).to eq({ digits: 0, letters: 0 })
+      it 'returns hash with digits=>0 and letters=>0' do
+        expect(subject).to eq({ digits: 0, letters: 0 })
       end
     end
 
-    context 'when string incorporates only symbols' do
+    context 'when logs incorporate only symbols' do
       let(:logs) { '____.' }
 
-      it 'returns hash with 0' do
-        expect(subject.task4(logs)).to eq({ digits: 0, letters: 0 })
+      it 'returns hash with digits=>0 and letters=>0' do
+        expect(subject).to eq({ digits: 0, letters: 0 })
       end
     end
 
-    context 'when string incorporates only digits' do
+    context 'when logs incorporate only digits' do
       let(:logs) { '12347' }
 
-      it 'returns hash with amount of numbers' do
-        expect(subject.task4(logs)).to eq({ digits: 5, letters: 0 })
+      it 'returns hash with digits=>5 and letters=>0' do
+        expect(subject).to eq({ digits: 5, letters: 0 })
       end
     end
 
-    context 'when string incorporates only letters' do
+    context 'when logs incorporate only letters' do
       let(:logs) { 'five' }
 
-      it 'returns hash with amount of numbers' do
-        expect(subject.task4(logs)).to eq({ digits: 0, letters: 4 })
+      it 'returns hash with digits=>0 and letters=>4' do
+        expect(subject).to eq({ digits: 0, letters: 4 })
       end
     end
 
-    context 'when string incorporates letters and digits' do
+    context 'when logs incorporate letters and digits' do
       let(:logs) { 'five5' }
 
-      it 'returns hash with amount of numbers' do
-        expect(subject.task4(logs)).to eq({ digits: 1, letters: 4 })
+      it 'returns hash with digits=>1 and letters=>4' do
+        expect(subject).to eq({ digits: 1, letters: 4 })
       end
     end
   end
