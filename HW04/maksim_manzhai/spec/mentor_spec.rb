97d8d81497c5f354cae66677d80b47d32f36b5ca
manzhaimaksim <maxbbb88@gmail.com>
2022-04-16 17:08:41 +0300

    editing tests for Homework4

--- HW04/maksim_manzhai/spec/mentor_spec.rb
+++ HW04/maksim_manzhai/spec/mentor_spec.rb
@@ -6,57 +6,48 @@ require 'homework'
 require 'notification'
 
 RSpec.describe Mentor do
-  let(:mentor) { described_class.new(name: 'Will', surname: 'Smith') }
+  subject { described_class.new(name: 'Will', surname: 'Smith') }
+
   let(:student) { Student.new(name: 'Chris', surname: 'Rock') }
   let(:notification) { Notification.new(title: 'HW03', description: 'OOP in Ruby') }
-  let(:homework) { Homework.new(title: 'HW03', description: 'OOP in Ruby', student: student, mentor: mentor) }
-
-  context 'when mentor created' do
-    it 'returns name' do
-      expect(mentor.name).to eql 'Will'
-    end
-
-    it 'returns surname' do
-      expect(mentor.surname).to eql 'Smith'
-    end
-  end
+  let(:homework) { Homework.new(title: 'HW03', description: 'OOP in Ruby', student: student, mentor: subject) }
 
   describe '#add_homework' do
     it 'returns an object of class Homework' do
-      homework = mentor.add_homework(title: 'HW03', description: 'OOP in Ruby', student: student)
-      expect(homework).to be_a(Homework)
+      expect(subject.add_homework(title: 'HW03', description: 'OOP in Ruby', student: student)).to be_a(Homework)
     end
   end
 
-  describe 'mark_as_read!' do
+  describe '#mark_as_read!' do
     let(:notification) { Notification.new(title: homework.title, description: 'Homework started') }
 
-    it 'mentor see notification as unreaded' do
-      mentor.notifications << notification
-      expect(mentor.notifications[0].readed).to be false
+    it 'sees empty array of notifications' do
+      expect(subject.notifications).to eq([])
     end
 
-    it 'mentor mark as read all notifications' do
-      mentor.notifications << notification
-      mentor.mark_as_read!
-      expect(mentor.notifications[0].readed).to be true
+    it 'gets notification' do
+      expect { subject.notifications << notification }.to(change { subject.notifications.size })
+    end
+
+    it 'marks as read all notifications' do
+      subject.notifications << notification
+      expect { subject.mark_as_read! }.to(change { subject.notifications[0].readed }.to(true))
     end
   end
 
   describe '#reject_to_work!' do
-    it 'mentor reject homework and student get notification' do
-      expect(mentor.reject_to_work!(homework).last).to be_a(Notification)
+    it 'rejects homework and student gets notification' do
+      expect(subject.reject_to_work!(homework).last).to be_a(Notification)
     end
   end
 
   describe '#accept!' do
-    it 'mentor accept homework' do
-      expect(mentor.accept!(homework).last).to be_a(Notification)
+    it 'accepts homework' do
+      expect(subject.accept!(homework).last).to be_a(Notification)
     end
 
-    it 'homework status becomes acceptable' do
-      homework = instance_double('homework', acceptable: true)
-      expect(homework.acceptable).to be true
+    it 'makes status of homework acceptable' do
+      expect(instance_double('homework', acceptable: true).acceptable).to be true
     end
   end
 end
--- HW04/maksim_manzhai/spec/student_spec.rb
+++ HW04/maksim_manzhai/spec/student_spec.rb
@@ -6,65 +6,53 @@ require 'mentor'
 require 'homework'
 
 RSpec.describe Student do
-  let(:student) { described_class.new(name: 'Chris', surname: 'Rock') }
+  subject { described_class.new(name: 'Chris', surname: 'Rock') }
+
   let(:mentor) { Mentor.new(name: 'Will', surname: 'Smith') }
   let(:notification) { Notification.new(title: 'HW03', description: 'OOP in Ruby') }
-  let(:homework) { Homework.new(title: 'HW03', description: 'OOP in Ruby', student: student, mentor: mentor) }
-
-  context 'when student created' do
-    it 'returns name' do
-      expect(student.name).to eql 'Chris'
-    end
-
-    it 'returns surname' do
-      expect(student.surname).to eql 'Rock'
-    end
-  end
+  let(:homework) { Homework.new(title: 'HW03', description: 'OOP in Ruby', student: subject, mentor: mentor) }
 
   context 'when student see notification' do
-    it 'returns notifications' do
-      student.notifications << notification
-      expect(student.notifications).to eql [notification]
+    it 'sees empty array of notifications' do
+      expect(subject.notifications).to eq([])
     end
 
-    it 'student mark as read all notifications' do
-      student.notifications << notification
-      student.mark_as_read!
-      expect(student.notifications[0].readed).to be true
+    it 'gets notification' do
+      expect { subject.notifications << notification }.to(change { subject.notifications.size })
+    end
+
+    it 'marks as read all notifications' do
+      subject.notifications << notification
+      expect { subject.mark_as_read! }.to(change { subject.notifications[0].readed }.to(true))
     end
   end
 
-  context 'when student see homeworks' do
-    it 'homeworks array is empty' do
-      expect(student.homeworks).to eql []
+  describe 'when student see array of homeworks' do
+    it 'is empty' do
+      expect(subject.homeworks).to eql []
     end
 
-    it 'student got homework' do
-      student.homeworks << homework
-      expect(student.homeworks).to eql [homework]
+    it 'gets homework' do
+      subject.homeworks << homework
+      expect(subject.homeworks).to eql [homework]
     end
   end
 
   describe '#to_work!' do
-    it 'student take to work a homework' do
-      expect(student.to_work!(homework)).to eql [homework]
+    it 'takes to work a homework' do
+      expect(subject.to_work!(homework)).to eql [homework]
     end
   end
 
   describe '#add_answer!' do
-    it 'array of answers for homework is empty' do
-      expect(homework.answers).to eql []
-    end
-
-    it 'student make answer for homework' do
-      student.add_answer!(homework, 'new students answer')
-      expect(homework.answers).to eql ['new students answer']
+    it 'makes answer for homework' do
+      expect { subject.add_answer!(homework, 'new students answer') }.to(change { homework.answers.size })
     end
   end
 
   describe '#to_check!' do
-    it 'student sent to check homework and mentor get notification' do
-      expect(student.to_check!(homework).last).to be_a(Notification)
+    it 'sents to check homework and mentor get notification' do
+      expect(subject.to_check!(homework).last).to be_a(Notification)
     end
   end
 end
