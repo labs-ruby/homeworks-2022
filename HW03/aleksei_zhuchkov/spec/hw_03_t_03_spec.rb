cddd4094544ca3defa0eb50d399968fc364b1649
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-04-18 01:01:33 +0300

    fix: refactor test task3 HW03

--- HW03/aleksei_zhuchkov/spec/hw_03_t_03_spec.rb
+++ HW03/aleksei_zhuchkov/spec/hw_03_t_03_spec.rb
@@ -6,35 +6,39 @@ RSpec.describe Homework3 do
   let(:obj_hw3) { described_class.new }
 
   describe '#task3' do
-    context 'when testing basic things' do
-      it 'returns value is string type' do
+    context 'when input data is empty' do
+      it 'returns the instance of string class' do
         expect(obj_hw3.task3('')).to be_an_instance_of(String)
       end
 
-      it 'returns value "0" (string) when get empty string' do
+      it 'returns the value "0"' do
         expect(obj_hw3.task3('')).to eq('0')
       end
     end
 
-    context 'when testing log with one right result' do
+    context 'when tested log has two right result' do
       let(:log) do
         <<~LOGS
           2018-12-31 23:59:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
           2018-12-31 23:59:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
           2018-12-31 23:59:49.8 ubuntu-xenial[14319] Debug - docker event processed
           2019-01-01 00:00:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
-          2019-01-01 00:00:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+          2019-01-01 00:00:49.7 ubuntu-xenial[14319] Debug - Calling core with action: messages
           2019-01-01 00:00:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
           2019-01-01 00:00:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
         LOGS
       end
 
-      it 'returns instance of String class' do
+      it 'returns the instance of String class' do
         expect(obj_hw3.task3(log)).to be_an_instance_of(String)
+      end
+
+      it 'returns the right value' do
+        expect(obj_hw3.task3(log)).to eq('60.0')
       end
     end
 
-    context 'when there is more than 2 valid event' do
+    context 'when tested log has more than two valid event' do
       let(:log) do
         <<~LOGS
           2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
@@ -48,8 +52,12 @@ RSpec.describe Homework3 do
         LOGS
       end
 
-      it 'returns instance of Array class' do
+      it 'returns the instance of Array class' do
         expect(obj_hw3.task3(log)).to be_an_instance_of(Array)
+      end
+
+      it 'returns the right value' do
+        expect(obj_hw3.task3(log)).to eq(['49.1', '60.0'])
       end
     end
   end
