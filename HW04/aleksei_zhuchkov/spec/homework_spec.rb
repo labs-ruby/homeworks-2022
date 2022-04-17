44eafe468fd2886eb488ebdbc95e4c49e835d63c
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-04-17 23:02:40 +0300

    fix: delete unnecessary code

--- HW04/aleksei_zhuchkov/spec/homework_spec.rb
+++ HW04/aleksei_zhuchkov/spec/homework_spec.rb
@@ -10,24 +10,6 @@ RSpec.describe Homework do
   let(:mentor) { Mentor.new(name: 'mentor_name', surname: 'mentor_surname') }
   let(:test_homework) { described_class.new('hw_title', 'hw_description', student, mentor) }
 
-  context 'when check information about student and mentor' do
-    it 'check right student name' do
-      expect(test_homework.student.name).to eq('student_name')
-    end
-
-    it 'check right student surname' do
-      expect(test_homework.student.surname).to eq('student_surname')
-    end
-
-    it 'check right mentor name' do
-      expect(test_homework.mentor.name).to eq('mentor_name')
-    end
-
-    it 'check right mentor surname' do
-      expect(test_homework.mentor.surname).to eq('mentor_surname')
-    end
-  end
-
   describe '#print_homework' do
     it 'method return instance of Array' do
       expect(test_homework.print_homework).to be_an_instance_of(Array)
