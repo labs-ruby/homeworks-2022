3a16f377f4bef8b64e4002a1170d3d250381284d
AlexeyShparun <94370593+AlexeyShparun@users.noreply.github.com>
2022-04-20 13:01:21 +0300

    Alexey: My hw 05 (#131)

---
+++ HW04/alexey_shparun/spec/mentor_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require_relative '../lib/homework'
+require_relative '../lib/mentor'
+require_relative '../lib/student'
+require_relative '../lib/notification'
+
+RSpec.describe Mentor do
+  let(:student) { Student.new(name: 'Alexey', surname: 'Shparun') }
+  let(:mentor) { described_class.new(name: 'Vasya', surname: 'Klapan') }
+
+  describe '#add_homework' do
+    let(:hw) { mentor.add_homework(title: 'HW04', description: 'description', student: student) }
+
+    context 'when arguments is correct' do
+      it 'return object of class' do
+        expect(hw).to be_a(Homework)
+      end
+    end
+
+    context 'when no arguments given' do
+      it 'returns ArgumentError' do
+        expect { mentor.add_homework }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#subscribe_to' do
+    context 'when mentor subscribe' do
+      it 'return student list' do
+        expect { mentor.subscribe_to!(student) }.to change(mentor, :student_list).to eq [student]
+      end
+    end
+  end
+
+  describe '#mark_as_read!' do
+    let(:hw) { mentor.add_homework(title: 'HW04', description: 'description', student: student) }
+
+    context 'when mentor marks as read' do
+      before do
+        student.to_work!(hw)
+      end
+
+      it 'return empty array' do
+        expect { mentor.mark_as_read! }.to change(mentor, :notifications_list).from([Notification]).to([])
+      end
+    end
+  end
+
+  describe '#accept!' do
+    let(:hw) { mentor.add_homework(title: 'HW04', description: 'description', student: student) }
+
+    context 'when mentor accept' do
+      it 'returns notifications with rejection' do
+        expect { mentor.accept!(hw) }.to change { student.notifications_list.length }.to(1)
+      end
+    end
+  end
+
+  describe '#reject_to_work!' do
+    let(:hw) { mentor.add_homework(title: 'HW04', description: 'description', student: student) }
+
+    context 'when mentor reject_to_work' do
+      it 'returns notifications with rejection' do
+        expect { mentor.reject_to_work!(hw) }.to change { student.notifications_list.length }.to(1)
+      end
+    end
+  end
+end
---
+++ HW04/alexey_shparun/spec/student_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require_relative '../lib/homework'
+require_relative '../lib/mentor'
+require_relative '../lib/student'
+require_relative '../lib/notification'
+
+RSpec.describe Student do
+  let(:title) { 'HW04' }
+  let(:description) { 'description' }
+  let(:student) { described_class.new(name: 'Alexey', surname: 'Shparun') }
+  let(:mentor) { Mentor.new(name: 'Vasya', surname: 'Klapan') }
+  let(:hw) { Homework.new(title, description, student, mentor) }
+
+  describe '#add_answer!' do
+    context 'when arguments is correct' do
+      it 'return object of class' do
+        expect(student.add_answer!(hw, 'some text')).to eq hw.answer_list
+      end
+    end
+
+    context 'when no arguments given' do
+      it 'returns ArgumentError' do
+        expect { student.add_answer! }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#to_work!' do
+    context 'when student take to work' do
+      it 'return object of class' do
+        expect { student.to_work!(hw) }.to change { student.notifications_list.length }.to(1)
+      end
+    end
+
+    context 'when no arguments given' do
+      it 'returns ArgumentError' do
+        expect { student.to_work! }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#to_check!' do
+    context 'when student ready to check' do
+      it 'return object of class' do
+        expect { student.to_check!(hw) }.to change { student.notifications_list.length }.to(1)
+      end
+    end
+
+    context 'when no arguments given' do
+      it 'returns ArgumentError' do
+        expect { student.to_check! }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#mark_as_read!' do
+    context 'when student marks as read' do
+      before do
+        student.to_work!(hw)
+      end
+
+      it 'return empty array' do
+        expect { student.mark_as_read! }.to change(student, :notifications_list).from([Notification]).to([])
+      end
+    end
+  end
+end
