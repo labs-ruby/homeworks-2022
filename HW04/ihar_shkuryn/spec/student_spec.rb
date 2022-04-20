7e1b475e4320cc23ad2361ff86750d39226e9f27
Igor <shkuryn@gmail.com>
2022-04-20 09:47:52 +0300

    rename obj to subject

--- HW04/ihar_shkuryn/spec/student_spec.rb
+++ HW04/ihar_shkuryn/spec/student_spec.rb
@@ -10,20 +10,20 @@ require 'byebug'
 RSpec.describe Student do
   let(:name) { 'John' }
   let(:surname) { 'Doe' }
-  let(:obj) { described_class.new(name: name, surname: surname) }
+  let(:subject) { described_class.new(name: name, surname: surname) }
 
   describe 'checking fields of instance' do
     context 'when fields is correct' do
       it 'returns instance of Student' do
-        expect(obj).to be_an_instance_of(described_class)
+        expect(subject).to be_an_instance_of(described_class)
       end
 
       it 'when name is correct' do
-        expect(obj.name).to eq(name)
+        expect(subject.name).to eq(name)
       end
 
       it 'when returns correct presentation' do
-        expect(obj.presentation).to eq("#{name} #{surname}")
+        expect(subject.presentation).to eq("#{name} #{surname}")
       end
     end
   end
@@ -31,7 +31,7 @@ RSpec.describe Student do
   describe '#notification' do
     context 'when student get notification' do
       it 'student notification is instance of array' do
-        expect(obj.notices).to be_an_instance_of(Array)
+        expect(subject.notices).to be_an_instance_of(Array)
       end
     end
   end
