d63d8099d0b1f0481d8c04d96a0971308af28e3f
agodes84 <renegad84@gmail.com>
2022-04-22 20:32:56 +0300

    HW03_T_04 suggested change

--- HW03/andrei_godes/spec/andrei_godes_hw_03_t_02_rspec.rb
+++ HW03/andrei_godes/spec/andrei_godes_hw_03_t_02_rspec.rb
@@ -7,7 +7,6 @@ RSpec.describe Homework3 do
   subject { described_class.new.task2(log) }
 
   context 'when the input is empty' do
-    
     subject { [] }
 
     it { is_expected.to eq([]) }
--- HW03/andrei_godes/spec/andrei_godes_hw_03_t_04_rspec.rb
+++ HW03/andrei_godes/spec/andrei_godes_hw_03_t_04_rspec.rb
@@ -4,23 +4,23 @@ require_relative '../andrei_godes_hw_03_t_04'
 require_relative 'spec_helper'
 
 RSpec.describe Homework3 do
-  subject { described_class.new }
+  subject { described_class.new.task4(string) }
 
   context 'when string consists of letters and digits' do
-    it 'returns number of letters and digits' do
-      expect(subject.task4('aefgh455fd67')).to eq({ digits: 5, letters: 7 })
-    end
+    let(:string) {'aefgh455fd67'}
+    it { is_expected.to eq({ digits: 5, letters: 7 })}
+  
   end
 
   context 'when string consists of letters, digits and symbols' do
-    it 'returns number of letters and digits' do
-      expect(subject.task4('1654bjh78-=0kjjhv')).to eq({ digits: 7, letters: 8 })
-    end
+    
+    let(:string) {'1654bjh78-=0kjjhv'}
+    it { is_expected.to eq({ digits: 7, letters: 8 })}
   end
 
   context 'when string is empty' do
-    it 'returns number 0 letters and 0 digits' do
-      expect(subject.task4('')).to eq({ digits: 0, letters: 0 })
-    end
+    
+    let(:string) {''}
+    it { is_expected.to eq({ digits: 0, letters: 0 })}
   end
 end
